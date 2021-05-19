331deb57d7097bf4a59d21e6a421d1860d0d7980
shankar-trainer <shankar7979@hotmail.com>
2021-05-20 02:24:33 +0530

    deleted

--- c ds/malloc/m3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int variable;
-    int *ptr = &variable;
-    *ptr = 20;
-    printf("%d", *ptr);
-    return 0;
-}
--- src/main/java/app1/calculator.java
+++
@@ -1,9 +0,0 @@
-package app1;
-
-public class calculator {
-
-	public float add(float a, float b) {
-		return a+b;
-	}
-	
-}
