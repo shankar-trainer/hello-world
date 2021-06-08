529e3550821b127adb51e7464095a75fa3fcd655
shankar kumar <shankar7979@hhotmail.com>
2021-06-08 18:09:21 +0530

    angular

--- EurekaServer/src/main/java/com/cts/EurekaServer.java
+++ EurekaServer/src/main/java/com/cts/EurekaServer.java
@@ -2,13 +2,13 @@ package com.cts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class BootMain {
+@EnableEurekaServer
+public class EurekaServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootMain.class, args);
+		SpringApplication.run(EurekaServer.class, args);
 	}
 }
--- SpringDataEx1/src/main/java/com/cts/web/ProductController.java
+++ SpringDataEx1/src/main/java/com/cts/web/ProductController.java
@@ -1,23 +1,24 @@
 package com.cts.web;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cts.dao.ProductRepository;
+//import com.cts.dao.ProductRepository;
 import com.cts.error.ProductNotFoundException;
 import com.cts.model.Product;
 import com.cts.service.ProductService;
@@ -25,6 +26,7 @@ import com.cts.service.ProductService;
 //@Controller
 @RestController
 @RequestMapping("/product")
+@CrossOrigin("http://localhost:4200")
 public class ProductController {
 
 	@Autowired
@@ -32,10 +34,9 @@ public class ProductController {
 
 	@RequestMapping("/all")
 	// @ResponseBody
-
 	public ResponseEntity<List<Product>> allProduct() {
 		try {
-			return new ResponseEntity<>(service.getAllProduct(), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(service.getAllProduct(), HttpStatus.OK);
 
 		} catch (ProductNotFoundException e) {
 			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
