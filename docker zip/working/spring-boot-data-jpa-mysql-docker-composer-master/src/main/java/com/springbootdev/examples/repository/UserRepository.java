98015c1e559e2f96b339bea2db13010dbadc828a
shankar kumar <shankar7979@hhotmail.com>
2021-06-23 03:59:43 +0530

    docker

--- docker zip/working/spring-boot-data-jpa-mysql-docker-composer-master/src/main/java/com/springbootdev/examples/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springbootdev.examples.repository;
-
-import com.springbootdev.examples.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long>
-{
-
-}
