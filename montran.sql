5d59bcc8ba7d19ab85317ea36756d2b90b3d09a7
shankar kumar <shankar7979@hhotmail.com>
2021-07-30 17:57:02 +0530

    sql

--- montran.sql
+++ montran.sql
@@ -75,6 +75,266 @@ insert into mobile (id,cost) values(78887,15345.4567766);
 select * from mobile;
 
 select * from employee; 
+ -- parent table 
+ create table customer_order(order_id int , order_name varchar(20) not null,cost float not null, cust_id int, 
+ primary key(order_id), foreign key (cust_id) references customer(cust_id));
+  
+ -- child table
+  create table customer(cust_id int , name varchar(20) not null, location varchar(20) not null default 'chennai',
+    primary key(cust_id));
+    
+desc customer;
+desc customer_order;
+ insert into customer_order values(1,'book order',1788, 1001);  -- first add record to child table  then add to parent
+  insert into customer_order values(2,'book order',1788, 1002);
+ insert into customer values(1001,'a kumar','madurai');    -- add record here then in parent table 
+ insert into customer values(1002,'a kumar','madurai');
+
+delete from customer where cust_id=1001;
+delete from customer_order where cust_id=1001; -- first delete from parent then child table 
+
+
+select * from customer; 
+select * from customer_order; 
+
+
+select * from customer where cust_id=1001; 
+
+-- aggregate function
+
+select * from employee;
+select count(*) from employee;
+select sum(salary) from employee;
+select sum(salary) as "total salary" from employee;
+select avg(salary) as "average salary" from employee;
+select max(salary) as "max salary" from employee;
+select min(salary) as "minimum salary" from employee;
+
+create table department (deptid int primary key, dept_name varchar(20), dept_loc varchar(20), no_of_employee int);
+insert into department values(9001,'transport','delhi', 20);
+insert into department values(9002,'training','noida', 30);
+insert into department values(9003,'development','delhi', 20);
+insert into department values(9004,'transport','chennai', 20);
+insert into department values(9005,'development','madurai', 20);
+insert into department values(9006,'development','delhi', 10);
+insert into department values(9007,'training','hyderabad', 20);
+insert into department values(9008,'development','hyderabad', 20);
+
+select   count(dept_name) , dept_name from department  group by  dept_name;
+select   count(dept_name) , dept_name, dept_loc from department  group by  dept_name;
+
+select   count(dept_loc) , dept_loc, dept_name from department  group by  dept_loc;
+select   count(dept_name) , dept_name,dept_loc from department  group by  dept_name having dept_name='transport';
+
+
+-- join 
+
+create table student (roll int primary key, name varchar(20) , location varchar(20));
+create table examination (roll int primary key, subject varchar(20) , marks varchar(20));
+
+insert into student values(1001, 'surendra kumar','delhi');
+insert into student values(1003, 'virendra kumar','delhi');
+insert into student values(1004, 'amar kumar','noida');
+insert into student values(1006, 'amit kumar','gurgaon');
+insert into student values(1007, 'jagadish kumar','delhi');
+
+insert into examination values(1001, 'math',90);
+insert into examination values(1002, 'science',80);
+insert into examination values(1005, 'social science',78);
+insert into examination values(1006, 'english',88);
+insert into examination values(1007, 'hindi',91);
+insert into examination values(1008, 'marathi',90);
+use montran;
+-- cartesian product
+select * from student,examination;
+-- equi join   old syntax  all  fields 
+select * from student,examination where student.roll=examination.roll;
+
+-- equi join   old syntax  using table name with fields
+select student.roll, student.name, student.location, examination.subject, examination.marks  
+ from student,examination where student.roll=examination.roll;
+
+-- equi join   old syntax  using table alias with fields
+select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s,examination e where s.roll=e.roll;
+
+-- equi join or inner  join ansi sql  syntax  using table alias with fields
+select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s inner join  examination e on s.roll=e.roll;
+
+-- left join 
+select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s left join  examination e on s.roll=e.roll;
+
+ -- left join list of student who have not appeared in examination
+
+ select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s left join  examination e on s.roll=e.roll where e.roll is null;
+-- right join 
+ select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s right join  examination e on s.roll=e.roll;
+
+select * from employee;
+
+create table  employee_1 as select * from employee where 1<0;
+ desc employee_1;
+ select * from employee_1;
+ alter table  employee_1 drop column dob;
+alter table  employee_1  add  designation varchar(20);
+alter table  employee_1  add  manager_id int;
+insert into employee_1 values (1001, 'suraj kumar',70000, 'clerk', 1003);
+insert into employee_1 values (1002, 'amit kumar ',60000, 'consultant', 1004);
+insert into employee_1 values (1003, 'sunil kumar ',90000, 'manager', null);
+insert into employee_1 values (1004, 'pravin kumar ',95000, 'manager', null);
+
+select * from employee_1;
+
+select * from employee_1 e1, employee_1 e2 where e1.emp_id=e2.manager_id;
+
+select e1.emp_id,e1.name, e1.salary ,  ' are manager of ' as '  ---->', e2.name   from employee_1 e1, 
+employee_1 e2 where e1.emp_id=e2.manager_id;
+
+-- sub query 
+select max(salary) from employee_1;
+select * from employee_1 where salary=95000;
+select * from employee_1; 
+ select * from employee_1 where salary=(select max(salary) from employee_1);
+ select * from employee_1 where salary=(select min(salary) from employee_1);
+
+-- using self  join 
+select e1.emp_id,e1.name, e1.salary ,  ' are manager of ' as '  ---->', e2.name   from employee_1 e1, 
+employee_1 e2 where e1.emp_id=e2.manager_id;
+-- same result using subquery 
+select emp_id,name, salary from employee_1 where emp_id in(select emp_id from employee_1 where 
+    designation='manager' ); 
+
+
+-- students who have not appered exam
+ select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s left join  examination e on s.roll=e.roll where e.roll is null;
+-- same output using subquery 
+ select s.roll, s.name, s.location 
+ from student s where s.roll not in( select distinct(roll) from examination);
+
+--    student appeared at exam  using inner join 
+select s.roll, s.name, s.location, e.subject, e.marks  
+ from student s inner join  examination e on s.roll=e.roll;
+-- same output using subquery 
+ select roll, name, location 
+ from student  where roll in( select distinct(roll) from examination);
+
+select roll from student  where  exists( select distinct(roll) from examination);
+select roll,name,location from student  where  exists( select distinct(roll) from examination);
+select roll,name,location from student  where  not exists( select distinct(roll) from examination);
+
+
+
+create table user1 (id int primary key, name varchar(20), city varchar(20));
+create table user2 (id int primary key, name varchar(20), city varchar(20));
+
+insert into user1 values(10001, 'a kumar' ,'chennai');
+insert into user1 values(10002, 'pankaj kumar' ,'madurai');
+insert into user1 values(10003, 'pawan kumar' ,'chennai');
+insert into user1 values(10004, 'sawan  kumar' ,'madurai');
+
+insert into user2 values(10001, 'a kumar' ,'chennai');
+insert into user2 values(10002, 'raman kumar' ,'chennai');
+insert into user3 values(10003, 'mohan kumar' ,'delhi');
+insert into user2 values(10004, 'janardan kumar' ,'noida');
+
+select * from user1;
+
+select * from user2;
+
+select * from user1
+union 
+select * from user2;
+
+select city from user1
+union 
+select city from user2;
+  --  view   create 
+create view employee_view as select * from employee;
+ -- execure view 
+select * from employee_view;
+ -- update view
+update   employee_view set name='laxmi kant' where emp_id=801;
+-- insert into view 
+ insert into employee_view values(8080,'suman parbhu',67000,'2000-11-11'); 
+ -- delete view 
+  delete from  employee_view where emp_id=802;
+  -- drop view 
+   drop view employee_view;
+
+create view union_view as  
+select * from user1
+union 
+select * from user2;
+
+select * from union_view;
+
+-- sequence  not supported in  mysql 
+-- h2.jar for sequence ,minus 
+
+create table employee( id int primary key, name varchar(20));
+-- create sequence emp_seq;
+insert into employee values(emp_seq.nextval, 'mahesh kumar');
+--  create sequence seq4 start with 1000;
+insert into employee values(seq4.nextval, 'mahesh kumar');
+select * from employee;
+delimiter ;
+drop procedure emp_search;
+delimiter //
+create procedure emp_search1(empid1 int ,out name1 varchar(20), out salary1 float)
+begin
+select name,salary  into name1,salary1 from employee where emp_id=empid1;
+end;
+//
+
+call emp_search1(802,@name,@salary);
+select @name, @salary;
+-- trigger 
+create table log(user varchar(20),time date);
+select * from  log;
+delimiter //
+create trigger emp_trg1 after insert on employee  for each row 
+begin
+insert into log values(current_user(), now());
+end;
+//
+select * from log//
+select * from employee;
+insert into employee values(6,'p k',8998899,'2001-10-10');
+
+delimiter ;
+create table employee_backup as select * from employee where 1<0;
+
+delimiter //
+
+create trigger emp_backup  after delete  on employee  for each row
+begin 
+insert into employee_backup values(old.emp_id, old.name,old.salary,old.dob);
+
+end ;
+//
+desc employee//
+ 
+select * from employee//
+desc employee;
+delete from employee//
+
+
+use montran;
+create table test1(dob date);
+desc test1;
+
+insert into test1 values('2020-10-10');
+
+select * from test1 where month(dob)=10;
+
+
+
+
 
 
 
