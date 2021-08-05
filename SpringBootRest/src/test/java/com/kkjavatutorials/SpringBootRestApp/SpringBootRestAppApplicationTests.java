96546a09adb7494fa9cdd1f0b0c50490574a04bb
shankar kumar <shankar7979@hotmail.com>
2021-08-05 05:39:38 +0530

    Montran

---
+++ SpringBootRest/src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkjavatutorials.SpringBootRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
