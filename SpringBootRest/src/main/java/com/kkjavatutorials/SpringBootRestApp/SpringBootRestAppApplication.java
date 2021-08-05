c53b3f642c1c6e32700c805ff2776de3e615127c
shankar kumar <shankar7979@hotmail.com>
2021-08-05 05:58:31 +0530

    Montran

---
+++ SpringBootRest/src/main/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.kkjavatutorials.SpringBootRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestAppApplication.class, args);
+	}
+
+}
