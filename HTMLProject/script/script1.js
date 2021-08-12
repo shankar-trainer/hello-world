c1ec41680b20994a30283222700f4d42cccb7324
shankar kumar <shankar7979@hotmail.com>
2021-08-12 17:13:23 +0530

    HTML

---
+++ HTMLProject/script/script1.js
@@ -0,0 +1,12 @@
+document.write('hello world')
+var a=10
+var b=20
+
+var c=a+b;
+document.write("<br>sum of "+a+" and "+b+" is   "+c)
+alert("no1 is "+a)
+alert("no2 is "+b)
+alert("sum  is "+c)
+alert("sum of "+a+" and "+b+" is   "+c)
+
+
