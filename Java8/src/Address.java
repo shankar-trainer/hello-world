b59930a5ce86bdf5432eb4e39ffa0af5c22fab89
shankar kumar <shankar7979@hotmail.com>
2021-09-14 13:28:08 +0530

    oop

---
+++ Java8/src/Address.java
@@ -0,0 +1,36 @@
+
+public class Address {
+
+	private String street;
+	private String city;
+	private String state;
+	private int pin;
+	
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	
+	
+	
+}
---
+++ Java8/src/Car.java
@@ -0,0 +1,28 @@
+
+public class Car {
+
+	private String carRegNo;
+	private String carModel;
+	private float carCost;
+	
+	public String getCarRegNo() {
+		return carRegNo;
+	}
+	public void setCarRegNo(String carRegNo) {
+		this.carRegNo = carRegNo;
+	}
+	public String getCarModel() {
+		return carModel;
+	}
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	public float getCarCost() {
+		return carCost;
+	}
+	public void setCarCost(float carCost) {
+		this.carCost = carCost;
+	}
+	
+	
+}
