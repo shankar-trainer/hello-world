bd6a90c554db92850b8b39efb9bf4f9821d1aa3e
shankar-trainer <shankar7979@hotmail.com>
2021-09-19 17:19:30 +0530

    mysql using gradle

--- docker-app/java-docker/gradle_java_app1/src/main/java/GradlePrj1/Program1.java
+++ docker-app/java-docker/gradle_java_app1/src/main/java/GradlePrj1/Program1.java
@@ -12,18 +12,19 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Program1 {
-
+ //docker inspect container id find IpAddress use it in program 
 	public static void main(String[] args) throws SQLException {
    
-		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-		Connection connection = DriverManager.getConnection
-		("jdbc:mysql://127.0.0.1:3306/db1", "root", "1234");
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                   //allowPublicKeyRetrieval=true&useSSL=false
+	
+Connection connection=DriverManager.getConnection("jdbc:mysql://172.17.0.2:3306/db1", "root", "1234");
+//	("jdbc:mysql://172.17.0.2:3306/db1?autoReconnect=true&useSSL=false", "root", "1234");
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("select * from employee");
 
 		while (resultSet.next())
-			System.out.println(resultSet.getInt(1) + "  " + resultSet.getString(2) + "  " + resultSet.getFloat("salary")
-					+ "  " + resultSet.getDate(4));
+			System.out.println(resultSet.getInt(1) + "  " + resultSet.getString(2) + "  " + resultSet.getFloat(3));
 
 	}
 }
