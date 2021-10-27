8ee936bc392b94359155948694d4e9397856d8ee
shankar kumar <shankar7979@hotmail.com>
2021-10-27 17:24:31 +0530

    springapp2

--- SpringApp2/src/main/java/BankMain.java
+++ SpringApp2/src/main/java/BankMain.java
@@ -8,7 +8,9 @@ public class BankMain {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Bank bank = ctx.getBean("bank", Bank.class);
+		Bank bank1 = ctx.getBean("bank", Bank.class);
 
+		System.out.println("Bank1 \n");
 		System.out.println(bank.getBankRegNo());
 		System.out.println(bank.getBankName());
 		System.out.println(bank.getBankLocation());
@@ -17,5 +19,17 @@ public class BankMain {
 		Manager m = bank.getManger();
 		System.out.println("\t" + m.getManagerId());
 		System.out.println("\t" + m.getManagerName());
+
+		System.out.println("\nBank2 \n");
+
+		System.out.println(bank1.getBankRegNo());
+		System.out.println(bank1.getBankName());
+		System.out.println(bank1.getBankLocation());
+		System.out.println("\nmanager \n");
+
+		m = bank1.getManger();
+		System.out.println("\t" + m.getManagerId());
+		System.out.println("\t" + m.getManagerName());
+
 	}
 }
