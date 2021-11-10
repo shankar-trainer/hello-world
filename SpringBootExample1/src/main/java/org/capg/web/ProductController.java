cb71faceb479ebe915af95f9fbb4a7ae6e18c313
shankar kumar <shankar7979@hotmail.com>
2021-11-10 15:21:35 +0530

    commit

--- SpringBootExample1/src/main/java/org/capg/web/ProductController.java
+++ SpringBootExample1/src/main/java/org/capg/web/ProductController.java
@@ -5,7 +5,13 @@ import java.util.List;
 import org.capg.model.Product;
 import org.capg.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +21,29 @@ public class ProductController {
 	@Autowired
 	ProductService service;
 	
-	@RequestMapping("/gettAll")
+	@GetMapping
+	//@RequestMapping("/getAll")
 	public List<Product> showAll(){
 	   return service.getAllProduct();	
 	}
+	
+	//@RequestMapping(value = "/add",method = RequestMethod.POST)
+	@PostMapping("/add")
+	public Product addProduct(@RequestBody Product p  ){
+		return service.addProduct(p);	
+	}
+	
+	//@RequestMapping(value = "/search/{id}")
+	@GetMapping(value = "/search/{id}")
+	public Product searchProduct(@PathVariable("id") int id  ){
+		return service.findProduct(id);	
+	}
+	
+	@RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
+	//@DeleteMapping(value = "/remove/{id}")
+	public Product deleteProduct(@PathVariable("id") int id  ){
+		return service.deleteProduct(id);	
+	}
+	
+	
 }
