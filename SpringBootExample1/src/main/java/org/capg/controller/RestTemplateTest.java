bf517a373ce265fec1e6d90c7e52917416ab3306
shankar kumar <shankar7979@hotmail.com>
2021-11-10 16:28:05 +0530

    commit

---
+++ SpringBootExample1/src/main/java/org/capg/controller/RestTemplateTest.java
@@ -0,0 +1,31 @@
+package org.capg.controller;
+
+import org.capg.model.Product;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateTest {
+
+	public static void main(String[] args) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		Product forObject = restTemplate.getForObject("http://localhost:9090/product/search/100", Product.class);
+		if (forObject != null) {
+			System.out.println("found ");
+			System.out.println(forObject.getId() + "   " + forObject.getPrdName() + "   " + forObject.getPrdCost());
+		} else
+			System.out.println("not found");
+
+		// String s= restTemplate.get("http://localhost:9090/product/getAll",
+		// Product.class);
+		Product prd1 = new Product();
+		prd1.setPrdCost(11111);
+		prd1.setPrdName("mobile");
+
+		Product p2 = restTemplate.postForObject("http://localhost:9090/product/add", prd1, Product.class);
+
+		if (p2 != null) {
+			System.out.println("added");
+			System.out.println(p2.getId() + "  " + p2.getPrdName() + "  " + p2.getPrdCost());
+		}
+	}
+}
