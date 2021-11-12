99134c67aaa1e80f1350cff1ea309742e105d599
shankar kumar <shankar7979@hotmail.com>
2021-11-12 18:10:17 +0530

    commit

---
+++ CoreJava/src/InfiniteLoop1.java
@@ -0,0 +1,16 @@
+
+public class InfiniteLoop1 {
+
+	public static void main(String[] args) {
+		/*
+		while(true)
+			System.out.println("hello");
+		*/
+		
+		while(1>0)
+			System.out.println("hello");
+		
+		
+		
+	}
+}
