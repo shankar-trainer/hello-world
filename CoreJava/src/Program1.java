3009e7d3412d334ee812f1ccb256a3e474596200
shankar kumar <shankar7979@hotmail.com>
2021-11-12 13:07:41 +0530

    commit

---
+++ CoreJava/src/Program1.java
@@ -0,0 +1,18 @@
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+		int x=0;
+		System.out.println("x is "+x);
+	
+		x=90;
+		
+		System.out.println("x is "+x);
+		
+		final int y=100;
+		//y=67767;
+		
+	}
+
+}
