de03ef72435b7ab90c98519a1b481dc738b80cb2
shankar-trainer <shankar7979@hotmail.com>
2022-02-01 10:38:33 +0530

    html

--- html/script/script2.js
+++ html/script/script2.js
@@ -4,19 +4,24 @@ function validate(){
 	 var age=document.getElementById("age")
 	 
 	 if(id.value==""){
-		  alert("id is blank")
+		  //alert("id is blank")
+		  document.getElementById("id_err").innerHTML="id is blank"
 	      return false;
 	 }
 	 else if(name.value==""){
-		  alert("name is blank")
-	      return false;
+		  //alert("name is blank")
+	      document.getElementById("name_err").innerHTML="name is blank"
+		  return false;
 	 }
 	 else if(name.value.length<=10){
-		  alert("invalid name it must of at least 10 chars")
-	      return false;
+		  //alert("invalid name it must of at least 10 chars")
+	      document.getElementById("name_err").innerHTML="invalid name must of  10 chars"
+		  return false;
 	 }
 	 else if(age.value==""){
-		  alert("age is blank")
+		  //alert("age is blank")
+		  document.getElementById("age_err").innerHTML="age"
+		  
 	      return false;
 	 }
 	 else
