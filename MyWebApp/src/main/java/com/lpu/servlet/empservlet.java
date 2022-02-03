e0da1c02105e7ed67afbcaaef2ad864ba1a1eb98
shankar-trainer <shankar7979@hotmail.com>
2022-02-03 18:39:13 +0530

    servlet

--- MyWebApp/src/main/java/com/lpu/servlet/empservlet.java
+++ MyWebApp/src/main/java/com/lpu/servlet/empservlet.java
@@ -20,12 +20,22 @@ public class empservlet extends HttpServlet {
 
 		String id = request.getParameter("id");
 		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String skill[] = request.getParameterValues("skill");
 
 		  response.setContentType("text/html");
 		  
 		out.println("<div style='background:pink;border:double;width:30%;padding:20px;margin-left:300px;'> " );
 		out.println("id is " + id);
 		out.println("<br>name is " + name);
+		out.println("<br><br>gender is " + gender);
+		out.println("<br><br>skill sets are following  ");
+		
+		out.println("<ol> " );
+		 for(String s:skill)
+			 out.println("<li> "+s );
+		
+		out.println("</ol> " );
 		out.println("</div> " );
 	}
 
--- MyWebApp/src/main/webapp/script/script1.js
+++ MyWebApp/src/main/webapp/script/script1.js
@@ -2,6 +2,32 @@ function form_validate(){
   var id=document.getElementById('id').value
   var name=document.getElementById('name').value
   var pass=document.getElementById('pass').value
+  var skill=document.getElementsByName('skill')
+  var gender=document.getElementsByName('gender');
+  
+  var gender_status=false;
+  var skill_status=false;
+ 
+    for(g of gender)
+    {
+     if(g.checked)
+     {
+     gender_status=true;
+     break;
+     } 
+    }
+ 
+    for(g of skill)
+    {
+     if(g.checked)
+     {
+     skill_status=true;
+     break;
+     } 
+    }
+ 
+ 
+ 
   if(id==''){
   alert('id is blank')
    return false;
@@ -14,6 +40,18 @@ function form_validate(){
   alert('name is blank')
    return false;
   }
+  
+ else if(gender_status==false){
+  alert('select the gender')
+   return false;
+  }
+  
+ else if(skill_status==false){
+  alert('select the skill set')
+   return false;
+  }
+  
+  
    return true;
    
 }
