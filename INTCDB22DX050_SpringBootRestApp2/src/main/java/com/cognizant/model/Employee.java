9abe23f47a018a04115bf410b186c1367dbcbe83
shankar-trainer <shankar7979@hotmail.com>
2022-05-04 06:33:05 +0530

    code

---
+++ INTCDB22DX050_SpringBootRestApp2/src/main/java/com/cognizant/model/Employee.java
@@ -0,0 +1,35 @@
+package com.cognizant.model;
+
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private float salary;
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+	
+	
+	
+}
