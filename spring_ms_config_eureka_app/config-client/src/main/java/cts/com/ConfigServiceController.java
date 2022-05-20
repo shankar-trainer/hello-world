e80689e529570e369d2573b24fba44c5074eb119
shankar-trainer <shankar7979@hotmail.com>
2022-05-20 13:33:18 +0530

    hello

--- spring_ms_config_eureka_app/config-client/src/main/java/cts/com/ConfigServiceController.java
+++ spring_ms_config_eureka_app/config-client/src/main/java/cts/com/ConfigServiceController.java
@@ -4,24 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class ConfigServiceController {
-	
-
-    @Value("${welcome.message}")
-
+  @Value("${welcome.message}")
 	String welcomeText;
-	@GetMapping("/go")
-	public String welcomeText() {
-		return welcomeText;
-	}
 
+		@GetMapping("/go")
+		public String welcomeText() {
+			return welcomeText;
+		}
 
-@GetMapping("/go1")
+  @GetMapping("/go1")
 	public String welcomeText1() {
 		return "<h2>Welcome to microservice</h2>";
 	}
-
-	
 }
--- spring_ms_config_eureka_app/config-client/src/main/java/cts/com/ServiceApplication.java
+++ spring_ms_config_eureka_app/config-client/src/main/java/cts/com/ServiceApplication.java
@@ -3,6 +3,8 @@ package cts.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://localhost:9090/go  -- will take values from development profile as bootstrap.properties has spring.profiles.active=dev
+
 @SpringBootApplication
 public class ServiceApplication {
 
