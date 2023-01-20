6415702c6401e9af0a0e473fe16d672492991a47
shankar-trainer <shankar7979@hotmail.com>
2023-01-20 12:11:42 +0530

    struts2

--- StrrusApp1/src/main/java/com/pathinfotech/model/User.java
+++ StrrusApp1/src/main/java/com/pathinfotech/model/User.java
@@ -2,7 +2,7 @@ package com.pathinfotech.model;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class User  extends ActionSupport{
+public class User extends ActionSupport {
 	private int userId;
 	private String userName;
 	private float userSalary;
@@ -10,27 +10,45 @@ public class User  extends ActionSupport{
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public float getUserSalary() {
 		return userSalary;
 	}
+
 	public void setUserSalary(float userSalary) {
 		this.userSalary = userSalary;
 	}
-	
+
 	@Override
 	public String execute() throws Exception {
-		if(getUserSalary()<=0||  getUserId()<=0|| getUserName().isEmpty())
+//		if(getUserSalary()<=0||  getUserId()<=0|| getUserName().isEmpty())
+//		{ 
+//			this.addActionError("error");
+//			return ERROR;
+//		}
+		if (getUserId() <= 0) {
+			addFieldError("userId", getText("id.required"));
 			return ERROR;
-		
+		} else if (getUserName().isEmpty()) {
+			addFieldError("userName", getText("name.required"));
+			return ERROR;
+		} else if (getUserSalary() <= 0) {
+			addFieldError("userSalary", getText("salary.required"));
+			return ERROR;
+		}
+
 		return SUCCESS;
 	}
 }
--- StrrusApp1/src/main/webapp/UserForm.jsp
+++ StrrusApp1/src/main/webapp/UserForm.jsp
@@ -3,19 +3,22 @@
 <%@taglib uri="/struts-tags" prefix="s" %>    
 <!DOCTYPE html>
 <html>
+<s:head/>
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
 </head>
 <body>
-<div style="border:solid;background:lightyellow;margin:auto;padding:20px;">
-<s:form action="userAction">
-<s:textfield label="Enter Id" name="userId"/><br>
-<s:textfield label="Enter Name"  name="userName"/><br>
-<s:textfield label="Enter Salary"  name="userSalary"/><br>
-<s:submit/>
-<s:reset/>
-</s:form>
+<s:actionerror/>
+
+<div style="border:solid;background:yellow;color:blue;margin:auto;padding:20px;width:30%;">
+	<s:form action="userAction">
+	<s:textfield label="Enter Id" name="userId"/><br>
+	<s:textfield label="Enter Name"  name="userName"/><br>
+	<s:textfield label="Enter Salary"  name="userSalary"/><br>
+	<s:submit/>
+	<s:reset/>
+	</s:form>
 </div>
 </body>
 </html>
