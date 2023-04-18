145aaeff30ced0c073c35f5a570dfb342e0ba082
shankar-trainer <shankar7979@hotmail.com>
2023-04-18 16:10:15 +0530

    added

---
+++ Corejava/src/Ticket.java
@@ -0,0 +1,33 @@
+
+public class Ticket {
+
+	private int ticketid;
+	private int price;
+	private static int availableTickets;
+	
+	public int getTicketid() {
+		return ticketid;
+	}
+	public void setTicketid(int ticketid) {  
+		this.ticketid = ticketid;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public static int getAvailableTickets() {
+		return availableTickets;
+	}
+	public static void setAvailableTickets(int availableTickets) {
+		   // only positive  values are allowed
+		Ticket.availableTickets = availableTickets;
+	}
+
+
+	public int calculateTicketCost(int nooftickets) {
+		  // no ticket available 
+		return -1;
+	}
+}
