1a842280bcea780c6749a51e5cfa4a0eefc040dd
shankar-trainer <shankar7979@hotmail.com>
2023-04-18 15:17:36 +0530

    added

--- Corejava/src/Passenger.java
+++ Corejava/src/Passenger.java
@@ -2,10 +2,37 @@
 public class Passenger {
 
 	private int passengerId;  // instance variable
-	private String passengerName;
-	private  int  passengerAge;
+	private String passengerName;// instance variable
+	private  int  passengerAge;// instance variable
+
 	private static String country="india";// class or static varible
-	
+
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public String getPassengerName() {
+		return passengerName;
+	}
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	public int getPassengerAge() {
+		return passengerAge;
+	}
+	public void setPassengerAge(int passengerAge) {
+		this.passengerAge = passengerAge;
+	}
+
+	public static String getCountry() {
+		return country;
+	}
+	public static void setCountry(String country) {
+		Passenger.country = country;
+	}
+
 	public static void main(String[] args) {
      Passenger passenger=new Passenger();
      passenger.passengerId=989898;
---
+++ Corejava/src/PassengerMain.java
@@ -0,0 +1,10 @@
+
+public class PassengerMain {
+
+	public static void main(String[] args) {
+	
+		System.out.println(Passenger.getCountry());
+	
+		
+	}
+}
