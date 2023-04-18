23e5b818c5f2b16eaf51b9d086fa62f7504529d0
shankar-trainer <shankar7979@hotmail.com>
2023-04-18 13:22:11 +0530

    added

--- ss.bat
+++ ss.bat
@@ -1,3 +1,3 @@
 cp -r "C:\Users\shankar\eclipse-workspace_chitkara\Corejava\src" Corejava
-
+cp  "C:\Users\shankar\Documents\notes.doc" .
 git add * && git commit -m "added" && git push origin chitkara_april23
