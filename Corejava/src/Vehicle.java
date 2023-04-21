fc9b94cab339f97a7e2b1e842aa9fea98977c1ce
shankar-trainer <shankar7979@hotmail.com>
2023-04-21 10:12:25 +0530

    added

--- Corejava/src/Vehicle.java
+++ Corejava/src/Vehicle.java
@@ -13,16 +13,18 @@ class Car extends Vehicle {  // level2
 }
 //class can extend only one class at a time
 //class SportsCar extends Car,Vehicle {  // error 
-
 class SportsCar extends Car {  // level3   
+	String location;
+
 	public SportsCar(String model, float cost, String location) {
 		super(model, cost);
 		this.location = location;
 	}
-
-	String location;
-
 	public static void main(String[] args) {
-
+          SportsCar car=new SportsCar("farari", 300000, "banglore");
+          System.out.println("sports car values ");
+          System.out.println("\tModel "+car.model);
+          System.out.println("\tCost "+car.cost);
+          System.out.println("\tLocation "+car.location);
 	}
 }
