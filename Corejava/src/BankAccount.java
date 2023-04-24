d09efdd9b720058c8f8c8ee6832f7b136ef3061f
shankar-trainer <shankar7979@hotmail.com>
2023-04-24 14:54:00 +0530

    added

---
+++ Corejava/src/BankAccount.java
@@ -0,0 +1,22 @@
+
+public class BankAccount {
+	int accountNumber;
+	float balance;
+	String ownerName;
+
+	public BankAccount(int accountNumber, float balance, String ownerName) {
+		super();
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.ownerName = ownerName;
+	}
+
+	void add(float amt) {
+		balance = balance + amt;
+	}
+	void subtract(float amt) {
+		balance = balance - amt;
+	}
+
+     
+}
