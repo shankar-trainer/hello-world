fa57ede5321e3e662eec43a4d6a344dcbc2afc5c
shankar-trainer <shankar7979@hotmail.com>
2023-04-24 14:36:22 +0530

    added

---
+++ Corejava/src/Overload2.java
@@ -0,0 +1,20 @@
+
+public class Overload2 {
+
+	void area(float length,float breadth) {
+		
+	}
+	void area(float radius) {
+		
+	}
+	
+	void area(float side1,float side2,float side3) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+}
