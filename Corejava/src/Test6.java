64e24508c2599bd33f00e18cd2996f6f8f932a1e
shankar-trainer <shankar7979@hotmail.com>
2023-05-03 08:58:53 +0530

    added

---
+++ Corejava/src/Test6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+		String choice = new Scanner(System.in).next();
+		System.out.println(choice == "y");
+		System.out.println(choice.equals("y"));
+	}
+}
