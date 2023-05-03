bd8ffb2ab334f6c542c205963aec4a7008674f3f
shankar-trainer <shankar7979@hotmail.com>
2023-05-03 14:53:02 +0530

    added

---
+++ Corejava/src/edu/chitraka/Instrument.java
@@ -0,0 +1,21 @@
+package edu.chitraka;
+
+public abstract  class Instrument {
+	abstract void  play();
+}
+
+class Piano extends Instrument{
+	@Override
+	void play() {
+	}
+}
+class Flute extends Instrument{
+	@Override
+	void play() {
+	}
+}
+class Guitar extends Instrument{
+	@Override
+	void play() {
+	}
+}
