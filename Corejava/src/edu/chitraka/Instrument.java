9b68520672cd4d67bb3a289d10ae8549c5d84dcf
shankar-trainer <shankar7979@hotmail.com>
2023-05-04 11:09:53 +0530

    added

--- Corejava/src/edu/chitraka/Instrument.java
+++ Corejava/src/edu/chitraka/Instrument.java
@@ -7,15 +7,18 @@ public abstract  class Instrument {
 class Piano extends Instrument{
 	@Override
 	void play() {
+		System.out.println("Piano is playing tan tan tan tan");
 	}
 }
 class Flute extends Instrument{
 	@Override
 	void play() {
+		System.out.println("Flute is playing toot toot toot toot");
 	}
 }
 class Guitar extends Instrument{
 	@Override
 	void play() {
+		System.out.println("Guitar is playing tin tin tin ");
 	}
 }
