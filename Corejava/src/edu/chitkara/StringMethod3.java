096b334fae0eb55d129fdecd99d4833d1cade5f6
shankar-trainer <shankar7979@hotmail.com>
2023-05-08 13:41:34 +0530

    added

--- Corejava/src/edu/chitkara/StringMethod3.java
+++ Corejava/src/edu/chitkara/StringMethod3.java
@@ -4,11 +4,19 @@ public class StringMethod3 {
 
 	public static void main(String[] args) {
 		String var1 = "we are learning java";
+System.out.println("after split array elements are  ");
+			
 		String[] split = var1.split(" ");
 		 for (String string : split) {
 			System.out.println(string);
 		}
 		
+		 System.out.println("after split array elements are  ");
+		 var1 = "we-all-are-indian";
+		 split = var1.split("-");
+		 for (String string : split) {
+			System.out.println(string);
+		} 
 
 	}
 }
