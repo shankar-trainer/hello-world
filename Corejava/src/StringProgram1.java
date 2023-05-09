2d9824d26188c17aee0b884c702e5a8228497b75
shankar-trainer <shankar7979@hotmail.com>
2023-05-09 10:53:35 +0530

    added

---
+++ Corejava/src/StringProgram1.java
@@ -0,0 +1,22 @@
+
+public class StringProgram1 {
+
+	public static void main(String[] args) {
+		
+		String var1="hello"; // same refernces
+		String var2="hello"; 
+		String var3=new String("hello"); // new Object 
+		
+		if(var1==var2)
+			 System.out.println(" var1== var2");
+		
+		if(var1==var3)
+			System.out.println(" var1== var3");  // == works with references
+
+		if(var1.equals(var3))
+			System.out.println(" var1 equals  var3");  // == works with references
+		
+		
+		
+	}
+}
---
+++ Corejava/src/StringProgram2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringProgram2 {
+public static void main(String[] args) {
+	
+	System.out.println("hello"=="hello"); // same object
+	System.out.println("hello"==new String("hello"));// different object 
+	System.out.println("hello".equals(new String("hello")));
+	
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter two names ");
+	
+	String s1=scanner.next(); 
+	String s2=scanner.next();
+	System.out.println("s1==s2" +s1==s2);
+	System.out.println("s1 equals s2" +(s1.equals(s2)));
+}
+}
