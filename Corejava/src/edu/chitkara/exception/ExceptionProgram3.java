99961bbf105d13f9585e3aa624420cea25031631
shankar-trainer <shankar7979@hotmail.com>
2023-05-10 09:40:26 +0530

    added

--- Corejava/src/edu/chitkara/exception/ExceptionProgram3.java
+++ Corejava/src/edu/chitkara/exception/ExceptionProgram3.java
@@ -1,30 +1,33 @@
 package edu.chitkara.exception;
 
 public class ExceptionProgram3 {
-
 	public static void main(String[] args) {
-		
-		String name="null";
-		String language="java";
 
+		String name = null;
+		String language = "java";
+                 //multiple try and multiple catch 
 		try {
-		System.out.println("name is "+name); 
-		System.out.println("name length is "+name.length()); 
-		System.out.println("language is "+language);
-		System.out.println("language 5th char is  "+language.charAt(4));
+			System.out.println("name is " + name);
+		System.out.println("name length is " + name.length());
 		}
-		
 		catch (NullPointerException e) {
 			System.err.println("null exception");
 		}
+		
+		try {
+			System.out.println("language is " + language);
+System.out.println("language 5th char is  " + language.charAt(4));
+		}
+		
 		catch (StringIndexOutOfBoundsException e) {
 			System.err.println(e.getMessage());
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.err.println("some error ");
 			e.printStackTrace();
 		}
-		
-		
+		 finally {
+			System.out.println(); 
+		 }
+
 	}
 }
