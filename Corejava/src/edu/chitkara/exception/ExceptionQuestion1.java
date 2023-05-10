ad82a4ca7fa805f8de16a70f5228725b5e63f0e5
shankar-trainer <shankar7979@hotmail.com>
2023-05-10 10:29:34 +0530

    added

---
+++ Corejava/src/edu/chitkara/exception/ExceptionQuestion1.java
@@ -0,0 +1,22 @@
+package edu.chitkara.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionQuestion1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			int x = scanner.nextInt();
+			int y = scanner.nextInt();
+			System.out.println(x / y);
+		} catch (InputMismatchException e) {
+			System.err.println(e.getClass().getName());
+		} catch (ArithmeticException e) {
+			System.err.println(e.getClass().getName());
+		}
+		p
+
+	}
+}
