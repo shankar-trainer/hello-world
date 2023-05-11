126fec985f46c250ee28c0505a5419cf43b4d7c4
shankar-trainer <shankar7979@hotmail.com>
2023-05-11 16:52:16 +0530

    added

--- Corejava/src/edu/chitkara/multithread/Program2.java
+++ Corejava/src/edu/chitkara/multithread/Program2.java
@@ -1,18 +1,19 @@
 package edu.chitkara.multithread;
 
-public class Program1 extends Thread {
+public class Program2 extends Thread {
+	int ar[]= {1,2,3,4,5,6,7,8};
 	@Override
 	public void run() {
-		for (int x = 1; x <= 20; x++)
-			System.out.println(x + "child thread  " + getName());
+		for (int x = 0; x <=7; x++)
+			System.out.println(x + "child thread  " + getName()+" "+ar[x]);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("main thread starts ");
 		System.out.println(Thread.currentThread().getName());
-		Thread thread1 = new Program1();
-		Thread thread2 = new Program1();
-		Thread thread3 = new Program1();
+		Thread thread1 = new Program2();
+		Thread thread2 = new Program2();
+		Thread thread3 = new Program2();
 		thread1.start();
 		thread2.start();
 		thread3.start();
