90c5efa76fc0bb022b7790164675b4e856370a5e
shankar-trainer <shankar7979@hotmail.com>
2023-05-11 11:04:12 +0530

    added

--- Corejava/src/edu/chitkara/exception/MyCalculator.java
+++ Corejava/src/edu/chitkara/exception/MyCalculator.java
@@ -1,7 +1,18 @@
 package edu.chitkara.exception;
 
 public class MyCalculator {
-//	long power(int, int) {
-//		
-//	}
+	static long power(int a , int b) {
+	      long pow=1;
+	      for (int i = 1; i <=b; i++) {
+			 pow=pow*a;
+		}
+	      return pow;
+	}
+	
+  public static void main(String args[]) {
+	System.out.println(power(2,0));  
+	System.out.println(power(1,0));  
+	System.out.println(power(0,1));  
+	System.out.println(power(2,3));  
+  }	
 }
