f2d562e766787f4b7b6028f18ac44a0a6bbe010a
shankar-trainer <shankar7979@hotmail.com>
2023-05-11 11:14:46 +0530

    added

--- Corejava/src/edu/chitkara/exception/MyCalculator.java
+++ Corejava/src/edu/chitkara/exception/MyCalculator.java
@@ -1,18 +1,31 @@
 package edu.chitkara.exception;
 
 public class MyCalculator {
-	static long power(int a , int b) {
-	      long pow=1;
-	      for (int i = 1; i <=b; i++) {
-			 pow=pow*a;
+	static long power(int a, int b) throws Exception {
+		long pow = 1;
+		if (a < 0 || b < 0)
+			throw new Exception("n or p should not be negative");
+
+		else if (a == 0 && b == 0)
+			throw new Exception("n or p should not be zero");
+		else {
+			for (int i = 1; i <= b; i++) {
+				pow = pow * a;
+			}
 		}
-	      return pow;
+		return pow;
 	}
-	
-  public static void main(String args[]) {
-	System.out.println(power(2,0));  
-	System.out.println(power(1,0));  
-	System.out.println(power(0,1));  
-	System.out.println(power(2,3));  
-  }	
+
+	public static void main(String args[]) {
+		try {
+			System.out.println(power(3, 5));
+			System.out.println(power(2, 4));
+			System.out.println(power(0, 0));
+			System.out.println(power(-1, -2));
+			System.out.println(power(-1, 3));
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+
+	}
 }
