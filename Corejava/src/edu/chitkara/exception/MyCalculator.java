ccbfa34956e1f9e01271763c416699bbb2fc0094
shankar-trainer <shankar7979@hotmail.com>
2023-05-11 11:31:56 +0530

    added

--- Corejava/src/edu/chitkara/exception/MyCalculator.java
+++ Corejava/src/edu/chitkara/exception/MyCalculator.java
@@ -1,5 +1,7 @@
 package edu.chitkara.exception;
 
+import java.util.Scanner;
+
 public class MyCalculator {
 	static long power(int a, int b) throws Exception {
 		long pow = 1;
@@ -17,15 +19,40 @@ public class MyCalculator {
 	}
 
 	public static void main(String args[]) {
-		try {
-			System.out.println(power(3, 5));
-			System.out.println(power(2, 4));
-			System.out.println(power(0, 0));
-			System.out.println(power(-1, -2));
-			System.out.println(power(-1, 3));
-		} catch (Exception e) {
-			System.err.println(e);
+		Scanner sc = new Scanner(System.in);
+		int ar[][] = new int[5][2];
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = 0; j < ar[0].length; j++) {
+				ar[i][j] = sc.nextInt();
+			}
+		}
+			try {
+				System.out.println(power(ar[0][0], ar[0][1]));
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+			try {
+				System.out.println(power(ar[1][0], ar[1][1]));
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+			try {
+				System.out.println(power(ar[2][0], ar[2][1]));
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+			try {
+				System.out.println(power(ar[3][0], ar[3][1]));
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+			try {
+				System.out.println(power(ar[4][0], ar[4][1]));
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+			
 		}
 
 	}
-}
+
