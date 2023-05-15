469ba0f00081672f01bdbc9f37803bfbedf39a05
shankar-trainer <shankar7979@hotmail.com>
2023-05-15 14:48:17 +0530

    added

--- Corejava/src/edu/chitkara/collection/BookMain.java
+++ Corejava/src/edu/chitkara/collection/BookMain.java
@@ -11,6 +11,7 @@ public class BookMain {
 		book[2] = new Book(44342, "java for dummies", "aman kumar", 900);
 		book[3] = new Book(44349, "java professional", "mahendra kumar", 1600);
 
+		System.out.println(" book add operation ");
 		LibraryManagement libraryManagement = new LibraryManagement();
 		libraryManagement.addBook(book[0]);
 		libraryManagement.addBook(book[1]);
@@ -19,12 +20,26 @@ public class BookMain {
 
 		System.out.println("all book information");
 		System.out.println("isbn\t\tname\tauthor\\ttcost");
-
+		
+		System.out.println(" book get all operation ");
 		List<Book> allBook = libraryManagement.getAllBook();
 		for (Book book2 : allBook) {
 			System.out.println(
 					book2.getIsbn() + "\t" + book2.getBname() + "\t" + book2.getAuthor() + "\t" + book2.getCost());
 		}
-
+		System.out.println(" book search  operation ");
+//             Book searchBook = libraryManagement.searchBook(book[0]);
+               Book searchBook = libraryManagement.searchBook(new Book(111,"java","s kumar",3000));
+	
+	           if(searchBook!=null) {
+	        	System.out.println("Book found ");
+	        	System.out.println();
+	        	System.out.println(
+						searchBook.getIsbn() + "\t" + searchBook.getBname() + "\t" + searchBook.getAuthor() + "\t" + searchBook.getCost());
+			
+	           }
+	           else
+	        	   System.out.println("Book not found ");
+	        	   
 	}
 }
