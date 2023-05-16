106fa763e4fcc05e69dd07be08a87c71d97632d2
shankar-trainer <shankar7979@hotmail.com>
2023-05-16 11:53:50 +0530

    added

---
+++ Corejava/src/edu/chitkara/collection/ListIteratorExample.java
@@ -0,0 +1,29 @@
+package edu.chitkara.collection;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class ListIteratorExample {
+
+	public static void main(String[] args) {
+		
+		int ar[]= {11,22,33,44,55};
+		
+		ArrayList<Integer> list1=new ArrayList<Integer>();
+		
+		for(int a:ar)
+			  list1.add(a);
+		
+		ListIterator<Integer> listIterator=list1.listIterator();
+            while(listIterator.hasNext())
+            	System.out.print(listIterator.next()+",");
+            
+            System.out.println("\n reverse direction ");
+            
+            while(listIterator.hasPrevious())
+            	System.out.print(listIterator.previous()+",");
+            	
+            
+		
+	}
+}
