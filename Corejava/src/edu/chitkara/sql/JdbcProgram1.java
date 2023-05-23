ce21e434dc2b5bd8694615acd09869d0a8945fc3
shankar-trainer <shankar7979@hotmail.com>
2023-05-23 17:24:08 +0530

    added

--- Corejava/src/edu/chitkara/sql/JdbcProgram1.java
+++ Corejava/src/edu/chitkara/sql/JdbcProgram1.java
@@ -2,20 +2,37 @@ package edu.chitkara.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcProgram1 {
 
-	public static void main(String[] args) throws SQLException {
-		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-		
-		Connection connection = DriverManager.getConnection
-				("jdbc:mysql://localhost:3306/chitkara","root","mysql");
-		if(connection!=null)
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		// 1st way to load driver
+		// DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+//2nd way to load driver    
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chitkara", "root", "mysql");
+		if (connection != null)
 			System.out.println("connection established");
-		else 
+		else
 			System.out.println("some problem");
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery("select * from employee");
+
+		while (rs.next())
+			System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getDouble(3));
+
+		ResultSet rs1 = st.executeQuery("select count(*) from employee");
+
+		rs1.next();
+		System.out.println("no of records present "+rs1.getInt(1));
+
+//		rs.next();
+		// System.out.println(rs.getInt("id") + "\t" + rs.getString(2) + "\t" +
+		// rs.getDouble("salary"));
+
 	}
-	
-	
+
 }
