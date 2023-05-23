e42bb5080957a9c5e458429f6c8e6911a7168c82
shankar-trainer <shankar7979@hotmail.com>
2023-05-23 11:21:51 +0530

    added

---
+++ Corejava/src/edu/chitkara/collection/Test2.java
@@ -0,0 +1,22 @@
+package edu.chitkara.collection;
+
+import java.util.ArrayList;
+
+public class Test2 {
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("ABCD");
+		list.add(1);
+		list.add(new Thread());
+		for (Object obj : list)
+			System.out.println(obj);
+	}
+}
+/*
+ warning -- and 
+
+ ABCD
+1
+Thread[Thread-0,5,main]
+
+*/
---
+++ chitkara.sql
@@ -0,0 +1,6 @@
+show databases;
+create database chitkara;
+use chitkara;
+show tables;
+create table employee(id int primary key , name varchar(20), salary double);
+desc employee;
