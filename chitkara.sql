04d5e4d97df934957cf689500f3f4801a2d62a29
shankar-trainer <shankar7979@hotmail.com>
2023-05-23 13:56:34 +0530

    added

--- chitkara.sql
+++ chitkara.sql
@@ -4,3 +4,6 @@ use chitkara;
 show tables;
 create table employee(id int primary key , name varchar(20), salary double);
 desc employee;
+insert into employee values(10001,'ram kumar', 20000);
+insert into employee values(10002,'shyam kumar', 30000);
+select * from employee;
--- ss.bat
+++ ss.bat
@@ -1,3 +1,5 @@
 cp -r "C:\Users\shankar\eclipse-workspace_chitkara\Corejava\src" Corejava
 cp  "C:\Users\shankar\Documents\notes.doc" .
+cp "C:\Users\shankar\Documents\chitkara.sql" .
+
 git add * && git commit -m "added" && git push origin chitkara_april23
