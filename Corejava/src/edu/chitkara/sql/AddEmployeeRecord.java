5755ce103ed07db3e0b3a87c60030948ffbdb0d5
shankar-trainer <shankar7979@hotmail.com>
2023-05-24 09:53:37 +0530

    added

--- Corejava/src/edu/chitkara/sql/AddEmployeeRecord.java
+++ Corejava/src/edu/chitkara/sql/AddEmployeeRecord.java
@@ -2,6 +2,7 @@ package edu.chitkara.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -11,18 +12,30 @@ public class AddEmployeeRecord {
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chitkara", "root",	"mysql");
-			
-	Statement st = connection.createStatement();
-int x = st.executeUpdate("insert into employee values(10004,'rahul ranjan',34000.0)");
-		   if(x==1)
-			   System.out.println(x+" Record added");
-		   
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chitkara", "root",
+					"mysql");
+			Scanner scanner = new Scanner(System.in);
+			int id;
+			String name;
+			double salary;
+			System.out.println("Enter id name and salary ");
+			id = scanner.nextInt();
+			scanner.nextLine();
+			name = scanner.nextLine();
+			salary = scanner.nextDouble();
 
+			Statement st = connection.createStatement();
+//int x = st.executeUpdate("insert into employee values(10004,'rahul ranjan',34000.0)");
+			int x = st.executeUpdate("insert into employee values(" + id + ",'" + name + "'," + salary + ")");
+			if (x == 1)
+				System.out.println(x + " Record added");
+
+			ResultSet rs = st.executeQuery("select * from employee");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getDouble(3));
+
+		} catch (ClassNotFoundException | SQLException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
