34741a90410ee24389aa3aff968ce114a94f3f0a
shankar-trainer <shankar7979@hotmail.com>
2023-07-26 12:03:10 +0530

    added

---
+++ SpringBootApp3/src/main/java/com/abc/MainApp1.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp1.class, args);
+	}
+}
---
+++ SpringBootApp3/src/main/java/com/abc/model/Employee.java
@@ -0,0 +1,19 @@
+package com.abc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	private int empId;
+	private String empName;
+	private float empSalary;
+	
+	
+}
