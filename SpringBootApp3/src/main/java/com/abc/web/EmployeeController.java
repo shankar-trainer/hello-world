1afa0431a47499598a0e9c106476d9ed6fbf5efb
shankar-trainer <shankar7979@hotmail.com>
2023-07-26 12:32:15 +0530

    added

--- SpringBootApp3/src/main/java/com/abc/web/EmployeeController.java
+++ SpringBootApp3/src/main/java/com/abc/web/EmployeeController.java
@@ -3,9 +3,11 @@ package com.abc.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,13 +27,12 @@ public class EmployeeController {
 		return service.searchEmployee(id);
 	}
 	
-	@GetMapping("/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	public Employee deleteEmployee(@PathVariable int id) {
 		return service.deleteEmployee(id);
 	}
 	
-	
-	@GetMapping("/delete/{id}")
+	@PutMapping("/update")
 	public Employee updateEmployee(@RequestBody Employee e) {
 		return service.updateEmployee(e);
 	}
@@ -40,7 +41,6 @@ public class EmployeeController {
 	public Employee addEmployee(@RequestBody Employee e) {
 		return service.addEmployee(e);
 	}
-	
 
 	@GetMapping("/all")
 	public List<Employee> getAllEmployee() {
