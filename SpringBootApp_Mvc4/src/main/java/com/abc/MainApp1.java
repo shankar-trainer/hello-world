cc8aabbf7431d3473801d714b535e7eaad37874c
shankar-trainer <shankar7979@hotmail.com>
2023-07-28 15:19:43 +0530

    added

--- SpringBootApp_Mvc4/src/main/java/com/abc/MainApp1.java
+++ SpringBootApp_Mvc4/src/main/java/com/abc/MainApp1.java
@@ -2,8 +2,10 @@ package com.abc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MainApp1 {
 
 	public static void main(String[] args) {
