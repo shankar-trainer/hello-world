8d7abab06183627f9b84db2a6aa53facd12269b3
shankar-trainer <shankar7979@hotmail.com>
2023-09-09 12:27:56 +0530

    added

---
+++ nodejs/addition.js
@@ -0,0 +1,11 @@
+var a=100;
+var b=200;
+var x=1000;
+addition=(p,q)=>p+q;
+
+module.exports={
+    a:a,
+    b:b,
+    xx1:x,
+    sum:addition
+};
---
+++ nodejs/addition_test.js
@@ -0,0 +1,7 @@
+var p=require('./addition')
+console.log(p.a)
+console.log(p.b)
+console.log(p.xx1)
+console.log(p.sum(p.a,p.b))
+
+console.log(`sum of ${p.a} and ${p.b} is ${p.sum(p.a,p.b)}`)
