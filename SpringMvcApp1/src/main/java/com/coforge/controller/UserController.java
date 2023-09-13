e64388608336ded15dac5a36752c6bf49d66598d
shankar-trainer <shankar7979@hotmail.com>
2023-09-13 16:55:57 +0530

    added

---
+++ SpringMvcApp1/src/main/java/com/coforge/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.coforge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "UserForm";
+	}
+}
