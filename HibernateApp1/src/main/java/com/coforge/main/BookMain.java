6613a06ef940dedd7e85cf91b9be75e14017fdcb
shankar-trainer <shankar7979@hotmail.com>
2023-09-15 15:14:00 +0530

    added

--- HibernateApp1/src/main/java/com/coforge/main/BookMain.java
+++ HibernateApp1/src/main/java/com/coforge/main/BookMain.java
@@ -1,31 +1,43 @@
 package com.coforge.main;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.coforge.model.Book;
 
 public class BookMain {
 
 	public static void main(String[] args) {
-		Configuration configuration=new Configuration().configure();
+		Configuration configuration = new Configuration().configure();
 		SessionFactory sf = configuration.buildSessionFactory();
-		
+
 		Session session = sf.openSession();
-		
-		 Transaction transaction = session.getTransaction();
-		
-		 Book book=new Book();
-		 book.setIsbn(10001);
-		 book.setBname("monkey story");
-		 book.setCost(100);
-		 
-		 transaction.begin();
-		 session.save(book);
-		 transaction.commit();
-		 System.out.println("Record added ");
+
+		Transaction transaction = session.getTransaction();
+
+		Book book = new Book();
+		book.setIsbn(10002);
+		book.setBname("tiger and rabbit story");
+		book.setCost(120);
+
+		transaction.begin();
+		session.save(book);
+		transaction.commit();
+		System.out.println("Record added ");
+		// hql -- hibernate query language , not table but pojo file name
+		Query createQuery = session.createQuery("from Book");
+		List<Book> list = createQuery.list();
+
+		list.forEach(System.out::println);
+
+		System.out.println("\n");
+		list.forEach(a -> System.out.println(a.getIsbn() + "\t" + a.getBname() + "\t" + a.getCost()));
+
 		session.close();
 	}
 }
