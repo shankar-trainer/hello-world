ea7d3e3deabdf3175e3872388083fb70bbda4d03
shankar-trainer <shankar7979@hotmail.com>
2023-09-15 16:22:09 +0530

    added

--- HibernateApp1/src/main/java/com/coforge/main/BookMain.java
+++ HibernateApp1/src/main/java/com/coforge/main/BookMain.java
@@ -21,12 +21,13 @@ public class BookMain {
 		Transaction transaction = session.getTransaction();
 
 		Book book = new Book();
-		book.setIsbn(10002);
-		book.setBname("tiger and rabbit story");
-		book.setCost(120);
+		book.setIsbn(10004);
+		book.setBname("king  and queeen story");
+		book.setCost(345.56f);
 
 		transaction.begin();
-		session.save(book);
+//		session.save(book);
+		session.persist(book);
 		transaction.commit();
 		System.out.println("Record added ");
 		// hql -- hibernate query language , not table but pojo file name
@@ -38,6 +39,11 @@ public class BookMain {
 		System.out.println("\n");
 		list.forEach(a -> System.out.println(a.getIsbn() + "\t" + a.getBname() + "\t" + a.getCost()));
 
+		Book find = session.find(Book.class, 10002);
+		if(find==null)
+			System.out.println("book not found with id 10002");
+		else 
+			System.out.println("book found with id 10002 \n"+find);
 		session.close();
 	}
 }
--- HibernateApp1/src/main/java/com/coforge/model/Book.java
+++ HibernateApp1/src/main/java/com/coforge/model/Book.java
@@ -1,15 +1,21 @@
 package com.coforge.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name="book1")
 public class Book {
 
 	@Id
+	@Column(length = 20, name = "book_isbn")
 	private long isbn;
+	@Column(length = 50, name = "book_bname", nullable = false)
 	private String bname;
+	@Column(length = 10, name = "book_cost", nullable = false, precision = 2)
 	private float cost;
 }
