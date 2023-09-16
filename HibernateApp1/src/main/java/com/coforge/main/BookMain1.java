8f692dc11ba1522a39e97e5d80f27ca815a2f6b7
shankar-trainer <shankar7979@hotmail.com>
2023-09-16 10:18:46 +0530

    added

--- HibernateApp1/src/main/java/com/coforge/main/BookMain1.java
+++ HibernateApp1/src/main/java/com/coforge/main/BookMain1.java
@@ -11,10 +11,18 @@ public class BookMain1 {
 
 	public static void main(String[] args) {
 		Session session = new Configuration().configure().buildSessionFactory().openSession();
-	    session.createNamedQuery("allbook").list().forEach(a->System.out.println(a));
-	  List<Book> booklist=  session.createNamedQuery("allbook").list();
-	
-	  System.out.println("\n");
-	    booklist.forEach(a->System.out.println(a.getIsbn()+"\t"+a.getBname()+"\t"+a.getCost()));
+		session.createNamedQuery("allbook").list().forEach(a -> System.out.println(a));
+		List<Book> booklist = session.createNamedQuery("allbook").list();
+
+		System.out.println("\n");
+		booklist.forEach(a -> System.out.println(a.getIsbn() + "\t" + a.getBname() + "\t" + a.getCost()));
+
+		System.out.println("native query");
+		List<Object[]> list1 = session.createNativeQuery("select * from book").list();
+
+		for (Object b[] : list1) {
+			System.out.println(b[0]+"   "+b[1]+"     "+b[2]);
+
+		}
 	}
 }
