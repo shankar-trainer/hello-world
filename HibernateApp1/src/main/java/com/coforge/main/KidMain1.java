b8ceb7267074f343dae332be266459cddecdb1e3
shankar-trainer <shankar7979@hotmail.com>
2023-09-16 12:33:59 +0530

    added

--- HibernateApp1/src/main/java/com/coforge/main/KidMain1.java
+++ HibernateApp1/src/main/java/com/coforge/main/KidMain1.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import com.coforge.model.one_to_one_uni.Kid;
 import com.coforge.model.one_to_one_uni.Toy;
 
-public class KidMain {
+public class KidMain1 {
 
 	public static void main(String[] args) {
 	
@@ -18,24 +18,22 @@ public class KidMain {
 	    toy.setToyCost(400.0f);
 	    toy.setToyName("drum master");
 	    
+	    
 	    kid.setAge(6);
 	    kid.setName("munnu kumar");
-	    kid.setToy(toy);
+	    
+	    toy.setKid(kid);
+	    //kid.setToy(toy);
 	    
 	    session.getTransaction().begin();
 	    
-	    // without cascading
-	    session.save(toy);
-	    session.save(kid);
-
 	    // with cascading
-	    session.save(kid);
+	    session.save(toy);
+	    //session.save(kid);
 	    
 	    session.getTransaction().commit();
 	    
-	    session.createQuery("from Kid").list().forEach(System.out::println);
 	
-	    System.out.println();
 	    session.createQuery("from Toy").list().forEach(System.out::println);
 	    
 	    
--- HibernateApp1/src/main/java/com/coforge/model/one_to_one_uni/Toy.java
+++ HibernateApp1/src/main/java/com/coforge/model/one_to_one_uni/Toy.java
@@ -1,8 +1,10 @@
 package com.coforge.model.one_to_one_uni;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
@@ -15,4 +17,8 @@ public class Toy {
 	private String toyName;
 	private String toyColor;
 	private Float toyCost;
+	
+	// bidirectional
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "toy")
+	private Kid kid;
 }
