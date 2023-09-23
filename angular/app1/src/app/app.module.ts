6c8f433061b8392b3128ec8ad41f94e9fa8c864f
shankar-trainer <shankar7979@hotmail.com>
2023-09-23 10:43:39 +0530

    added

--- angular/app1/src/app/app.module.ts
+++ angular/app1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent
+    StudentComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/app1/src/app/employee/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+
+    id:number;
+    name:string;
+    salary:number;
+
+    constructor(id:number,name:string,salary:number){
+     this.id=id;
+     this.name=name;
+     this.salary=salary;
+    }
+}
