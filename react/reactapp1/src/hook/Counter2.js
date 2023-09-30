8eff620d364ce14ae6485a6c2cf90e735540c424
shankar-trainer <shankar7979@hotmail.com>
2023-09-30 15:36:44 +0530

    added

--- react/reactapp1/src/hook/Counter2.js
+++ react/reactapp1/src/hook/Counter2.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 export default function Counter2() {
     const [count1, setCount] = useState(0);
     const [count2, decrementCount] = useState(100);
-    const [id, changeId] = useState('0');
+    const [id, changeId] = useState('76');
+    const [err_id, setErrorId] = useState('0');
     const [name, changeName] = useState('shyam kumar');
+    const [err_name, setErrorName] = useState('');
     const [age, changeAge] = useState('10');
 
     return (
-        <div className="container border border-primary bg-info text-light p-5 w-50 m-5">
+        <div className="container border border-primary bg-info text-light p-5 w-75 m-5">
             <div className="form-group">
                 <table>
                     <tr>
@@ -22,23 +24,50 @@ export default function Counter2() {
                     </tr>
                 </table>
 
-                <form className="bg-secondary w-50 p-5 m-5">
+                <form className="bg-secondary w-100 p-5 m-5">
                     <div className="form-group">
-                      <label>Enter Id</label>
-                      <input className="form-control" onChange={()=>changeId()}></input>
+                        <label>Enter Id</label>
+                        <input className="form-control" onChange={(e) => {
+                            if (e.target.value == '')
+                                setErrorId('id is blank')
+                            else if (e.target.value <= 0)
+                                setErrorId('id  cannot be zero or -ve')
+                            else if (isNaN(e.target.value))
+                                setErrorId('id must be numeric only')
+                            else {
+                                setErrorId('')
+                                changeId(e.target.value)
+                            }
+                        }} />
+                        <div className="text-warning">{err_id}</div>
                     </div>
                     <div className="form-group">
-                      <label>Enter Name</label>
-                      <input className="form-control"></input>
+                        <label>Enter Name</label>
+                        <input className="form-control" onChange={
+                            (e) => {
+                                if (e.target.value == '')
+                                    setErrorName('name is blank')
+
+                                else if (e.target.value.length < 5 || e.target.value.length > 15)
+                                    setErrorName('name length must be of 5 - 15 characters')
+                                else {
+                                    setErrorName('')
+                                    changeName(e.target.value)
+                                }
+                            }
+
+                        }></input>
+                        <div className="text-warning">{err_name}</div>
+
                     </div>
-                     
+
                     <div className="form-group">
-                      <label>Enter Age</label>
-                      <input className="form-control"></input>
+                        <label>Enter Age</label>
+                        <input className="form-control"></input>
                     </div>
-                     
+
                 </form>
-        </div>
+            </div>
         </div>
     )
 }
