548bc32b716d2ec4ca95a8443fb15faf50f50473
shankar-trainer <shankar7979@hotmail.com>
2023-09-30 10:27:25 +0530

    added

--- react/reactapp1/src/UserForm1.js
+++ react/reactapp1/src/UserForm1.js
@@ -5,46 +5,36 @@ class UserForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: '',
-            name: '',
-            id_error: '',
-            name_error: ''
+            id: 10001,
+            name: 'suresh kumar'
         }
     }
     id_change = (event) => {
-        if (event.target.value == '')
-            this.setState({ id_error: 'id is blank' })
-        else
-            this.setState({ id: event.target.value })
+        this.setState({ id: event.target.value })
     }
     name_change = (event) => {
-        if (event.target.value == '')
-            this.setState({ name_error: 'name is blank' })
-        else
-            this.setState({ name: event.target.value })
+        this.setState({ name: event.target.value })
     }
 
     render() {
         return (
-            <div className="border border-primary p-5 m-5 w-50 bg-info">
+            <div className="border border-primary p-5 m-5 w-50">
                 <form>
                     <div className="form-group">
                         <label>Enter Id</label>
                         <input className="form-control" onChange={this.id_change}></input>
-                        <span>{this.state.id_error}</span>
                     </div>
 
                     <div className="form-group">
                         <label>Enter Name</label>
                         <input className="form-control" onChange={this.name_change}></input>
-                        <span>{this.state.name_error}</span>
                     </div>
 
                     <div className="form-group">
                     </div>
                 </form>
 
-                <div className="border border-primary p-5 mt-5 bg-secondary text-light">
+                <div className="border border-primary">
                     <p>id is {this.state.id}</p>
                     <p>name is {this.state.name}</p>
 
