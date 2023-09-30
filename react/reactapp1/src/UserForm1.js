3f02106ca4ce9b9eb44c3ec1df764a836f4392ef
shankar-trainer <shankar7979@hotmail.com>
2023-09-30 10:30:31 +0530

    added

--- react/reactapp1/src/UserForm1.js
+++ react/reactapp1/src/UserForm1.js
@@ -5,36 +5,52 @@ class UserForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: 10001,
-            name: 'suresh kumar'
+            id: '',
+            name: '',
+            id_error: '',
+            name_error: ''
         }
     }
     id_change = (event) => {
-        this.setState({ id: event.target.value })
+        if (event.target.value == '')
+            this.setState({ id_error: 'id is blank' })
+     else if (isNaN(event.target.value))
+            this.setState({ id_error: 'id must be number only' })
+        else
+            this.setState({ id: event.target.value })
     }
     name_change = (event) => {
-        this.setState({ name: event.target.value })
+        if (event.target.value == '')
+            this.setState({ name_error: 'name is blank' })
+            
+        else if (event.target.value.length<5)
+            this.setState({ name_error: 'name must of minimum 5 charcater' })
+
+        else
+            this.setState({ name: event.target.value })
     }
 
     render() {
         return (
-            <div className="border border-primary p-5 m-5 w-50">
+            <div className="border border-primary p-5 m-5 w-50 bg-info">
                 <form>
                     <div className="form-group">
                         <label>Enter Id</label>
-                        <input className="form-control" onChange={this.id_change}></input>
+                        <input className="form-control" onClick={this.id_change}></input>
+                        <span>{this.state.id_error}</span>
                     </div>
 
                     <div className="form-group">
                         <label>Enter Name</label>
-                        <input className="form-control" onChange={this.name_change}></input>
+                        <input className="form-control" onClick={this.name_change}></input>
+                        <span>{this.state.name_error}</span>
                     </div>
 
                     <div className="form-group">
                     </div>
                 </form>
 
-                <div className="border border-primary">
+                <div className="border border-primary p-5 mt-5 bg-secondary text-light">
                     <p>id is {this.state.id}</p>
                     <p>name is {this.state.name}</p>
 
--- react/reactapp1/src/readme.txt
+++ react/reactapp1/src/readme.txt
@@ -8,11 +8,3 @@ create react application
 cd  create-react-app reactapp1
 
 npm start
-
-
--- previous application having no dependencies(node_module)
-
-  npm install  -- install from package.json 
-    
-	-- if no package.json then ue below command 
-  npm install react-scripts --save   
