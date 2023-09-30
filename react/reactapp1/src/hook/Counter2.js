889d88ee5fe70f9d104b6c6150103a533f07d755
shankar-trainer <shankar7979@hotmail.com>
2023-09-30 16:14:25 +0530

    added

--- react/reactapp1/src/hook/Counter2.js
+++ react/reactapp1/src/hook/Counter2.js
@@ -7,67 +7,84 @@ export default function Counter2() {
     const [err_id, setErrorId] = useState('0');
     const [name, changeName] = useState('shyam kumar');
     const [err_name, setErrorName] = useState('');
-    const [age, changeAge] = useState('10');
+    const [age, setAge] = useState('10');
+    const [err_age, setErrorAge] = useState('10');
 
     return (
-        <div className="container border border-primary bg-info text-light p-5 w-75 m-5">
-            <div className="form-group">
-                <table>
-                    <tr>
-                        <td><label>Increment   Count is {count1}</label></td>
-                        <td><input type='button' onClick={() => setCount(count1 + 1)} value="increment" /></td>
-                    </tr>
-                    <tr>
-                        <td><label>Decrement   Count is {count2}</label></td>
-                        <td><input type='button' onClick={() => decrementCount(count2 - 1)} value="decrement" />
-                        </td>
-                    </tr>
-                </table>
+        <div className="container border border-primary bg-info text-light p-5 w-100 m-5">
+            <table className="table   table-dark p-5 m-5 w-50">
+                <tr>
+                    <td><label>Increment   Count is {count1}</label></td>
+                    <td><input type='button' onClick={() => setCount(count1 + 1)} value="increment" /></td>
+                </tr>
+                <tr>
+                    <td><label>Decrement   Count is {count2}</label></td>
+                    <td><input type='button' onClick={() => decrementCount(count2 - 1)} value="decrement" />
+                    </td>
+                </tr>
+            </table>
 
-                <form className="bg-secondary w-100 p-5 m-5">
-                    <div className="form-group">
-                        <label>Enter Id</label>
-                        <input className="form-control" onChange={(e) => {
+            <form className="bg-secondary w-100 p-5 m-5">
+                <div className="form-group">
+                    <label>Enter Id</label>
+                    <input className="form-control" onChange={(e) => {
+                        if (e.target.value == '')
+                            setErrorId('id is blank')
+                        else if (e.target.value <= 0)
+                            setErrorId('id  cannot be zero or -ve')
+                        else if (isNaN(e.target.value))
+                            setErrorId('id must be numeric only')
+                        else {
+                            setErrorId('')
+                            changeId(e.target.value)
+                        }
+                    }} />
+                    <div className="text-warning">{err_id}</div>
+                </div>
+                <div className="form-group">
+                    <label>Enter Name</label>
+                    <input className="form-control" onChange={
+                        (e) => {
                             if (e.target.value == '')
-                                setErrorId('id is blank')
-                            else if (e.target.value <= 0)
-                                setErrorId('id  cannot be zero or -ve')
-                            else if (isNaN(e.target.value))
-                                setErrorId('id must be numeric only')
+                                setErrorName('name is blank')
+
+                            else if (e.target.value.length < 5 || e.target.value.length > 15)
+                                setErrorName('name length must be of 5 - 15 characters')
                             else {
-                                setErrorId('')
-                                changeId(e.target.value)
+                                setErrorName('')
+                                changeName(e.target.value)
                             }
-                        }} />
-                        <div className="text-warning">{err_id}</div>
-                    </div>
-                    <div className="form-group">
-                        <label>Enter Name</label>
-                        <input className="form-control" onChange={
-                            (e) => {
-                                if (e.target.value == '')
-                                    setErrorName('name is blank')
+                        }
 
-                                else if (e.target.value.length < 5 || e.target.value.length > 15)
-                                    setErrorName('name length must be of 5 - 15 characters')
-                                else {
-                                    setErrorName('')
-                                    changeName(e.target.value)
-                                }
+                    }></input>
+                    <div className="text-warning">{err_name}</div>
+
+                </div>
+
+                <div className="form-group">
+                    <label>Enter Age</label>
+                    <input className="form-control"
+                        onChange={(e) => {
+                            if (e.target.value == '')
+                                setErrorAge('age can not be blank')
+
+                            else if (e.target.value < 0)
+                                setErrorAge('age can not be 0 or -ve')
+
+                            else if (isNaN(e.target.value))
+                                setErrorAge('age must of number ')
+                            else {
+                              setErrorAge('')
+                              setAge(e.target.value)
                             }
 
-                        }></input>
-                        <div className="text-warning">{err_name}</div>
+                        }}
+                    ></input>
+                    <div className="text-warning">{err_age}</div>
 
-                    </div>
+                </div>
 
-                    <div className="form-group">
-                        <label>Enter Age</label>
-                        <input className="form-control"></input>
-                    </div>
-
-                </form>
-            </div>
+            </form>
         </div>
     )
 }
