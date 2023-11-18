452ee1e796eff33952df219e6bfda2b6b8630751
shankar-trainer <shankar7979@hotmail.com>
2023-11-18 17:48:16 +0530

    added

--- coupling_di/spring/usingxml/SpringLoose3/src/main/java/ex1/loose/Main.java
+++ coupling_di/spring/usingxml/SpringLoose3/src/main/java/ex1/loose/Main.java
@@ -2,11 +2,13 @@ package ex1.loose;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(Employee.class);
-        
+	public static void main(String[] args) {
+		// ApplicationContext ctx = new
+		// AnnotationConfigApplicationContext(Employee.class);
+
 //        ApplicationContext ctx_home = new AnnotationConfigApplicationContext(HomeAddress.class);
 //        ApplicationContext ctx_office = new AnnotationConfigApplicationContext(OfficeAddress.class);
 //        
@@ -18,5 +20,14 @@ public class Main {
 //        employee.setAddress(ctx_office.getBean(OfficeAddress.class));
 //        employee.getAddress().addressDetail();
 //        
-    }
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+		Employee employee = ctx.getBean("office_employee", Employee.class);
+		employee.getAddress().addressDetail();
+
+		employee = ctx.getBean("home_employee", Employee.class);
+		employee.getAddress().addressDetail();
+
+	}
 }
