acb3af03dd6878150101dd2404d368a20d78f1b4
shankar-trainer <shankar7979@hotmail.com>
2023-12-30 15:17:54 +0530

    added

--- coforge3/SpringMvcWebApp1/src/main/java/com/coforge/model/Employee.java
+++ coforge3/SpringMvcWebApp1/src/main/java/com/coforge/model/Employee.java
@@ -1,12 +1,30 @@
 package com.coforge.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import lombok.Data;
 
 @Data
 public class Employee {
 
-	private int empId;
+	@Min(value = 10000, message = "id must be minimum 10000")
+	@Max(value = 100000, message = "id must be maximum 100000")
+	private Integer empId;
+
+	@NotNull(message = "name is null")
+	@NotEmpty(message = "name is empty")
+	@NotBlank(message = "name is blank")
+	@Size(min = 5, max = 20, message = "name must of 5 - 20 chars")
 	private  String empName;
-	private float empSalary;
+	
+
+	@NotNull(message = "salary is null")
+	private Float empSalary;
 	
 }
