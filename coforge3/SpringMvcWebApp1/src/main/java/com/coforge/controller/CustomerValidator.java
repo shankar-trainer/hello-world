2763ccd99069583da4629464f8ccaf773128ff9e
shankar-trainer <shankar7979@hotmail.com>
2023-12-30 16:52:32 +0530

    added

--- coforge3/SpringMvcWebApp1/src/main/java/com/coforge/controller/CustomerValidator.java
+++ coforge3/SpringMvcWebApp1/src/main/java/com/coforge/controller/CustomerValidator.java
@@ -23,8 +23,15 @@ public class CustomerValidator implements org.springframework.validation.Validat
 		Customer customer=(Customer) target;
 		if(customer.getCustomerId()<=0)
 			errors.rejectValue("customerId", "id.required","id is required");
-		if(customer.getCustomerSalary()<=0)
+		
+		else if(customer.getCustomerId()<10000 || customer.getCustomerId()>50000)
+			errors.rejectValue("customerId", "id.range","id should between 10000 - 50000");
+		
+		else if(customer.getCustomerSalary()<=0)
 			errors.rejectValue("customerSalary","salary.required", "salary is required");
+		
+		else if(customer.getCustomerSalary()<50000 || customer.getCustomerSalary()>500000)
+			errors.rejectValue("customerSalary","salary.range", "salary should between 50000 - 500000");
 		
 	}
 
