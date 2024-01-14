b9a758103a66ca7f2010417be38276c735b3ca81
shankar-trainer <shankar7979@hotmail.com>
2024-01-14 13:54:39 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -1,14 +1,8 @@
-rem cp -r C:\angular_project\Angular_crud\src  "angular spring boot\Angular_crud"
-
-rem  cp -r C:\Users\Training\eclipse-workspace1\SpringEurekaServer .
-rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp1 .
- rem  cp -r C:\Users\Training\Downloads\SpringBootRestApp1\SpringBootRestApp1 .
-
-rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp2_Profiles .
-rem cp -r C:\Users\Training\eclipse-workspace1\SpringConfigClient .
 rem cp -r C:\Users\Training\eclipse-workspace1\SpringConfigServer .
 
 rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp_RestTemplate .
+
+rem git remote set-url origin https://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@github.com/shankar-trainer/hello-world
 
 
 cp -r C:\react_project\reactapp1\src react\reactapp1
