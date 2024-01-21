72f2d3c51cd1d76cc795cd2a0388741ee0f2a9c0
shakar-trainer <shakar7979@hotmail.com>
2024-01-21 13:43:02 +0530

    first commit

--- ss.bat
+++ ss.bat
@@ -5,7 +5,7 @@ rem cp -r C:\Users\Training\eclipse-workspace1\SpringRestJpaApp_RestTemplate .
 rem git remote set-url origin https://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@github.com/shankar-trainer/hello-world
 
 
-cp -r C:\react_project\reactapp1\src react\reactapp1
+rem cp -r C:\react_project\reactapp1\src react\reactapp1
 
  git add *
   git commit -m "first commit"
