b134858af033c77b6182c4d55f0b9c08e9cd2215
shakar-trainer <shakar7979@hotmail.com>
2024-06-16 21:17:28 +0530

    first commit

---
+++ spring_security/spring_security_app1/src/main/java/SecurityConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends SecurityConfigurerAdapter {
+
+
+}
