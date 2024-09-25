c07faa980c5f29ddcbbd48ea0d0977c0c391bcd8
shakar-trainer <shakar7979@hotmail.com>
2024-09-25 06:08:09 +0530

    first commit

---
+++ coforge_sep_2024/spring/spring-mvc/src/main/java/com/coforge/config/MyWebMVCConfig.java
@@ -0,0 +1,50 @@
+package com.coforge.config;
+
+import org.springframework.context.*;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.*;
+import org.springframework.validation.*;
+import org.springframework.validation.beanvalidation.*;
+import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.coforge.controller","com.coforge.dao"})
+//@ComponentScan("com.coforge.controller")
+public class MyWebMVCConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+    @Bean
+
+    public MessageSource messageSource() {
+
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+
+        source.setBasename("message");
+
+        return source;
+
+    }
+
+
+
+
+    @Override
+    public Validator getValidator() {
+
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+
+        validator.setValidationMessageSource(messageSource());
+
+        return validator;
+
+    }
+
+}
---
+++ coforge_sep_2024/spring/spring-mvc/src/main/webapp/WEB-INF/jsp/UserForm.jsp
@@ -0,0 +1,16 @@
+<h1>User Form</h1>
+<%@ page import="com.coforge.model.Product"%>
+<%@ page import="java.util.List"%>
+<%@ page import="com.coforge.dao.ProductDaoImpl"%>
+<%@ page import="com.coforge.dao.ProductDao"%>
+<%@ page import="org.springframework.beans.factory.annotation.Autowired"%>
+
+<%!
+@Autowired
+ProductDao dao;
+
+%>
+<%
+List<Product> list1=   dao.getAllProduct();
+   list1.forEach(System.out::println);
+%>
