1b8399961c2e4fe3740d1bde2f44edfa0a1e3159
shankar-trainer <shankar7979@hotmail.com>
2025-10-13 12:54:56 +0530

    added

--- angular-crud/src/app/addcustomer/model/customer1.ts
+++
@@ -1,6 +0,0 @@
-export interface Customer1 {
-    name: string;
-    salary: number;
-    dob: string;
-
-}
--- angular-crud/src/app/allcustomer/model/customer.ts
+++
@@ -1,7 +0,0 @@
-export interface Customer{
-    id:number;
-    name:string;
-    salary:number;
-    dob:string;
-    
-}
