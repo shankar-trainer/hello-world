9a59f2fcc76b93c34e292a0c7d7e148497ee38e5
shankar-trainer <shankar7979@hotmail.com>
2025-11-13 11:21:23 +0530

    added

--- angular/ngrx child bootstrap retry interceptor jsonserver/ngrx child bootstrap interceptor/src/app/crud/service/book.service.ts
+++ angular/ngrx child bootstrap retry interceptor jsonserver/ngrx child bootstrap interceptor/src/app/crud/service/book.service.ts
@@ -11,8 +11,8 @@ export class BookService {
   constructor(private http: HttpClient) { }
 
     getallbook1(): Observable<any> {
-        //  return this.http.get<Book[]>("http://localhost:3000/books")
-        return this.http.get<Book[]>("http://localhost:7070/Book").pipe(
+         return this.http.get<Book[]>("http://localhost:3000/books").pipe(
+        // return this.http.get<Book[]>("http://localhost:7070/Book").pipe(
             retry(3), // Retry the request up to 3 times
             catchError((error: HttpErrorResponse) => {
                 console.error(error + " retry error ......");
@@ -22,8 +22,8 @@ export class BookService {
     }
 
   getallbook(): Observable<any> {
-    //  return this.http.get<Book[]>("http://localhost:3000/books")
-    return this.http.get<Book[]>("http://localhost:7070/Book")
+     return this.http.get<Book[]>("http://localhost:3000/books")
+    // return this.http.get<Book[]>("http://localhost:7070/Book")
   }
 
   addbook(b: Book): Observable<Book> {
