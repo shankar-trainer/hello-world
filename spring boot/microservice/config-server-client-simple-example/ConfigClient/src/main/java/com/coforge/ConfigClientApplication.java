fa7c9722999be5c6ff4bccc28767970202ce7c3a
shankar-trainer <shankar7979@hotmail.com>
2026-02-23 17:12:23 +0530

    added

---
+++ spring boot/microservice/config-server-client-simple-example/ConfigClient/src/main/java/com/coforge/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.coforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
+
+//http://localhost:9090/employee
---
+++ spring boot/microservice/config-server-client-simple-example/ConfigServer/src/main/java/com/coforge/model/Employee.java
@@ -0,0 +1,36 @@
+package com.coforge.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="employee-service")
+public class Employee {
+
+	private int id;
+	private String name;
+	private float salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
