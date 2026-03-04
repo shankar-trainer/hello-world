dea6b8e3bc14c42dd10313a167498bc0bfac3a22
shankar-trainer <shankar7979@hotmail.com>
2026-03-04 12:29:00 +0530

    added

---
+++ spring boot/load balancing/Spring-Cloud-Microservice-gateway-loadbalancing/eureka-server/src/main/java/com/purbarun/ecom/cofiguration/EurekaServerConfiguration.java
@@ -0,0 +1,9 @@
+package com.purbarun.ecom.cofiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerConfiguration {
+
+}
---
+++ spring boot/load balancing/Spring-Cloud-Microservice-gateway-loadbalancing/inventory-service/src/main/java/com/purbarun/ecom/controller/InventoryController.java
@@ -0,0 +1,37 @@
+package com.purbarun.ecom.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.purbarun.ecom.model.GreetingResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+	@Value("${eureka.instance.instance-id}")
+	private String instanceId;
+	@Value("${spring.application.name}")
+	private String appName;
+	@Autowired
+	private EurekaClient eurekaClient;
+
+	@GetMapping("/greet")
+	public ResponseEntity<GreetingResponse> getProduct(HttpServletRequest request) {
+		InstanceInfo service = eurekaClient.getApplication(appName).getInstances().get(0);
+		GreetingResponse response = new GreetingResponse();
+		response.setGreeting("Hello from Inventory service !!");
+		response.setInstanceid(instanceId);
+		response.setPort(service.getPort());
+		response.setUrl(request.getRequestURL().toString());
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+}
