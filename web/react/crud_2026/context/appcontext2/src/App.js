daa9b26792b25ffd21cfac158aa5739ccb6f0e83
shankar-trainer <shankar7979@hotmail.com>
2026-03-19 10:54:41 +0530

    added

--- web/react/crud_2026/context/appcontext2/src/App.js
+++ web/react/crud_2026/context/appcontext2/src/App.js
@@ -1,28 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import {UserComponent} from "./usercontext/UserComponent";
-import {UserDsiplay} from "./usercontext/UserDisplay";
-import {UserProvider} from "./usercontext/UseProvider";
-import {EmployeeProvider} from "./usercontext1/EmployeeProvider";
-import {EmployeeComponent1} from "./usercontext1/EmployeeComponent1";
-import {AuthProvider} from "./usercontext2/AuthProvider";
-import {LoginButton} from "./usercontext2/LoginButton";
-
+import { UserComponent } from "./usercontext/UserComponent";
+import { UserDsiplay } from "./usercontext/UserDisplay";
+import { UserProvider } from "./usercontext/UseProvider";
+import { EmployeeProvider } from "./usercontext1/EmployeeProvider";
+import { EmployeeComponent1 } from "./usercontext1/EmployeeComponent1";
+import { AuthProvider } from "./usercontext2/AuthProvider";
+import { LoginButton } from "./usercontext2/LoginButton";
+import { Articles } from './reducer/article/components/Articles';
+import { AddArticle } from './reducer/article/components/AddArticle';
+import { ArticleProvider } from './reducer/article/components/ArticleProvider';
+import { ItemProvider } from './reducer/reducer1/ItemProvider';
+import { ItemInput } from './reducer/reducer1/ItemInput';
+import { ItemList } from './reducer/reducer1/ItemList';
+// reducer/article\\omponents\AddArticle.js
 function App() {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row border border-primary p-5 m-5">
                 <UserProvider>
                     <div className="col-6">
-                         <UserComponent/>
+                        <UserComponent />
                     </div>
                     <div className="col-6">
-                        <UserDsiplay/>
+                        <UserDsiplay />
                     </div>
                 </UserProvider>
             </div>
 
-            <div className="row">
+            <div className="row border border-primary p-5 m-5">
                 <EmployeeProvider>
                     <EmployeeComponent1>
 
@@ -31,15 +37,31 @@ function App() {
                 </EmployeeProvider>
 
             </div>
-            <div className="row">
+            <div className="row border border-primary p-5 m-5">
                 <AuthProvider>
                     <LoginButton />
                 </AuthProvider>
 
             </div>
-
-
+            <h2>using reducer and context both below </h2>
+            <div className="row border border-primary p-5 m-5">
+                <ArticleProvider>
+                    <AddArticle></AddArticle>
+                    <Articles></Articles>
+                </ArticleProvider>
             </div>
+            <div className="row border border-primary p-5 m-5">
+                <ItemProvider>
+                    <div className="col-6">
+                        <ItemInput></ItemInput>
+                    </div>
+                    <div className="col-6">
+                        <ItemList></ItemList>
+                    </div>
+
+                </ItemProvider>
+            </div>
+        </div>
     );
 }
 
---
+++ web/react/crud_2026/context/appcontext2/src/article/components/Article.txt
@@ -0,0 +1,9 @@
+export const Article = ({ article }) => {
+    return (
+        <>
+            <p>Id :{article.id}</p>
+            <h6>Title :{article.title}</h6>
+            <p>Body :{article.body}</p>
+        </>
+    )
+}
