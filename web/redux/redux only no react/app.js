d4c8ffa251e442b7b7e902695ee2bb9f3cb03b5e
shankar-trainer <shankar7979@hotmail.com>
2026-03-26 16:46:55 +0530

    added

---
+++ web/redux/redux only no react/app.js
@@ -0,0 +1,17 @@
+//https://github.com/redux-book/starter
+import { createStore } from 'redux';
+
+const reducer = (state, action) =>
+    action.type === 'INC'
+        ? state + 1
+        : state;
+
+const initialState = 0;
+
+const store = createStore(reducer, initialState);
+
+store.subscribe(() => document.getElementById('counter').innerText = store.getState());
+
+setInterval(() => store.dispatch({ type: 'INC' }), 500);
+
+console.log("Redux started");
