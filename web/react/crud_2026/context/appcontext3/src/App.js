c72ca27411c04d1bfb845fde19443489a5bbb44c
shankar-trainer <shankar7979@hotmail.com>
2026-03-30 11:36:58 +0530

    added

--- web/react/crud_2026/context/appcontext3/src/App.js
+++ web/react/crud_2026/context/appcontext3/src/App.js
@@ -8,10 +8,11 @@ import UserForm from './reducer1/UserForm';
 import SignupForm from './reducer1/SignupForm';
 import UserForm1 from './reducer1/UserForm1';
 import SignupForm1 from './reducer1/SignupForm1';
+import { User, User1, User2 } from './destructure/User';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      {/* <UserProvider>
          <UserInput></UserInput>
          <UserDisplay></UserDisplay>
@@ -19,10 +20,18 @@ function App() {
         <CountDisplay></CountDisplay> */}
         {/* <UserForm></UserForm> */}
         {/* <SignupForm></SignupForm> */}
-        <SignupForm1></SignupForm1>
+        {/* <SignupForm1></SignupForm1> */}
         {/* <UserForm1></UserForm1> */}
+        <User  id={10019} name={'ram'}></User>
+        <User1  id={10001} name={'ram'}></User1>
+        <User2  {...cars}></User2>
     </div>
   );
 }
 
+
+
+ const cars=[
+    "maruti","alto","hundai"
+ ]
 export default App;
