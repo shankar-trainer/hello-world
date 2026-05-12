059ec3ce7a7d94446605d89dab3c122329b2ca3f
shankar-trainer <shankar7979@hotmail.com>
2026-05-12 12:35:27 +0530

    added

---
+++ php/project1/oop/_get _set Employee.php
@@ -0,0 +1,27 @@
+<?php  
+
+class Employee{
+	
+	private $data=[];
+	
+	public function __set($name,$value){
+		$this->data[$name]=$value;
+	}  
+	
+	public function __get($name){
+	 return $this->data[$name];
+	} 
+}
+
+$emp=new Employee();
+$emp->id=10001;
+$emp->name="amit kumar";
+$emp->salary=20000;
+
+echo "employee data ";
+
+echo "\nid is ".$emp->id;
+echo "\nname is ".$emp->name;
+echo "\nsalary is ".$emp->salary;
+
+?>
