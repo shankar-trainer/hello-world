4a229018c6549080a387eb1e946aa844157aa060
shankar-trainer <shankar7979@hotmail.com>
2026-05-21 16:46:46 +0530

    added

--- php/phpproject/database/add_record_form.php
+++ php/phpproject/database/add_record_form.php
@@ -6,37 +6,55 @@
     <title>Document</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
-
-
 </head>
 <body>
 <div class="container w-75 border border-primary p-5 m-5">
-<form action="add_record1.php" method="post">
-<h2>Add Record</h2>
-    <div class="md-3 row">
-        <label class="col-6">Enter Id</label>
-        <input type="number" name="id" class="form-control col-6">
-    </div>
-    <div class="md-3 row">
-        <label class="col-6">Enter Name</label>
-        <input  name="name" class="form-control col-6">
-    </div>
-    <div class="md-3 row">
-        <label class="col-6">Enter Salary</label>
-        <input type="number" name="salary" class="form-control col-6">
-    </div>
-    <div class="md-3 row">
-        <label class="col-6">Enter Dob</label>
-        <input type="date" name="dob" class="form-control col-6" >
-    </div>
-    <div>
-        <div>
-            <button type="submit" class="btn btn-secondary mt-3">submit</button>
-            <button type="reset" class="btn btn-secondary mt-3">cancel</button>
+<form action="add_record1.php" method="post" class="container mt-4">
+    <h2>Add Record</h2>
+    
+    <!-- Changed md-3 to mb-3 -->
+    <div class="mb-3 row align-items-center">
+        <div class="col-6">
+            <label class="form-label mb-0">Enter Id</label>
+        </div>
+        <div class="col-6">
+            <input type="number" name="id" class="form-control">
         </div>
     </div>
 
+    <div class="mb-3 row align-items-center">
+        <div class="col-6">
+            <label class="form-label mb-0">Enter Name</label>
+        </div>
+        <div class="col-6">
+            <input name="name" class="form-control">
+        </div>
+    </div>
+
+    <div class="mb-3 row align-items-center">
+        <div class="col-6">
+            <label class="form-label mb-0">Enter Salary</label>
+        </div>
+        <div class="col-6">
+            <input type="number" name="salary" class="form-control">
+        </div>
+    </div>
+
+    <div class="mb-3 row align-items-center">
+        <div class="col-6">
+            <label class="form-label mb-0">Enter Dob</label>
+        </div>
+        <div class="col-6">
+            <input type="date" name="dob" class="form-control">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-end">
+            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            <button type="reset" class="btn btn-secondary mt-3">Cancel</button>
+        </div>
+    </div>
 </form>
 </div>    
 
