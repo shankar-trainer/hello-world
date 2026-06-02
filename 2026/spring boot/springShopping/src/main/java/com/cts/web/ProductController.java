62f910ee679d555cdf41cf0287a2ed316d7c7525
shankar-trainer <shankar7979@hotmail.com>
2026-06-02 15:23:26 +0530

    added

--- 2026/spring boot/springShopping/src/main/java/com/cts/web/ProductController.java
+++ 2026/spring boot/springShopping/src/main/java/com/cts/web/ProductController.java
@@ -2,6 +2,7 @@ package com.cts.web;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cts.model.Product;
 import com.cts.model.User;
@@ -27,24 +29,43 @@ public class ProductController {
 		return "productform";
 	}
 
-	List<Product> plist; 
+	List<Product> plist;
+
 	@PostMapping("/productsubmit")
-	public String submitform(@ModelAttribute @Valid Product product, BindingResult result, HttpSession session
-			) {
+	public String submitform(@ModelAttribute @Valid Product product, BindingResult result, HttpSession session) {
 
 		if (result.hasErrors())
 			return "productform";
 		else {
-			List<Product>	 plist=(List<Product>)session.getAttribute("plist");
-			if(plist==null) {
-				plist=new ArrayList<Product>();
+			List<Product> plist = (List<Product>) session.getAttribute("plist");
+			if (plist == null) {
+				plist = new ArrayList<Product>();
 				plist.add(product);
 				session.setAttribute("plist", plist);
-			}else {
-			plist.add(product);
-			session.setAttribute("plist", plist);
+			} else {
+				plist.add(product);
+				session.setAttribute("plist", plist);
 			}
 			return "productpayment";
 		}
+	}
+
+	@GetMapping("/remove")
+	public String removeProductById(@RequestParam("id") int id, HttpSession session) {
+		List<Product> plist = (List<Product>) session.getAttribute("plist");
+		if (plist != null) {
+			Optional<Product> first = plist.stream().filter(p -> p.getPrdid() == id).findFirst();
+			Product p1 = first.get();
+			plist.remove(p1);
+		}
+		return "productpayment";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+//	 return "logout";
+		return "productpayment";
+
 	}
 }
