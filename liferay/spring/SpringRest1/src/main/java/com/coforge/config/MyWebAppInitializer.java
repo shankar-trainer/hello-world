aad92d40b9b67d6719a5f3b4b9c1fad7866aa0fc
shankar-trainer <shankar7979@hotmail.com>
2026-07-23 09:48:06 +0530

    added

---
+++ liferay/spring/SpringRest1/src/main/java/com/coforge/config/MyWebAppInitializer.java
@@ -0,0 +1,9 @@
+package com.coforge.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override protected Class<?>[] getRootConfigClasses() { return null; }
+    @Override protected Class<?>[] getServletConfigClasses() { return new Class<?>[] { WebConfig.class }; }
+    @Override protected String[] getServletMappings() { return new String[] { "/" }; }
+}
