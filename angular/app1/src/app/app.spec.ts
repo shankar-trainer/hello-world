9627ef50b312a6fc1b24e56ed2defaccf508934a
shankar-trainer <shankar7979@hotmail.com>
2026-08-04 11:15:40 +0530

    added

--- angular/app1/src/app/app.spec.ts
+++ angular/app1/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, App1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app1');
   });
 });
--- angular/app1/src/app/employee/employee.ts
+++ angular/app1/src/app/employee/employee.ts
@@ -8,16 +8,4 @@ import { Component } from '@angular/core';
 })
 export class Employee {
 
-  id:number;
-  name:string;
-  age:number;
-  dob:Date;
-
-  constructor(){
-    this.id=1898989;
-    this.name="dr aryan sahu";
-    this.age=22;
-    this.dob=new Date('2002-10-22');
-  }
-
 }
