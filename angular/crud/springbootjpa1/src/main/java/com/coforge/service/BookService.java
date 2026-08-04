732fdd81f13e56623cb90ad0cf32c1d549318030
shankar-trainer <shankar7979@hotmail.com>
2026-08-04 14:12:40 +0530

    added

--- angular/crud/springbootjpa1/src/main/java/com/coforge/service/BookService.java
+++ angular/crud/springbootjpa1/src/main/java/com/coforge/service/BookService.java
@@ -1,15 +1,10 @@
 package com.coforge.service;
 
 import com.coforge.exception.BookException;
-import com.coforge.model.Author;
 import com.coforge.model.Book;
-import com.coforge.model.Publisher;
-import com.coforge.repository.AuthorRepository;
 import com.coforge.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Set;
