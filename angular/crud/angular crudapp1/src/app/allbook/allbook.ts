acd08bea6a707ea8655cab5fadb56325aba13fc5
shankar-trainer <shankar7979@hotmail.com>
2026-08-08 00:46:50 +0530

    added

--- angular/crud/angular crudapp1/src/app/allbook/allbook.ts
+++ angular/crud/angular crudapp1/src/app/allbook/allbook.ts
@@ -11,25 +11,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './allbook.css',
 })
 export class Allbook {
-  // b: Observable<Book[]>;
-  //1.// b: Observable<any>;
-  //2.//
-  // b: Book[] = [];
   b!: any;
-  msg!: string;
+  msg: string='';
   constructor(private service: BookService) {
-    //1.//this.b = this.getallbook();
 
     this.service.getallbook().subscribe(data => {
       this.b = data;
+      console.log(JSON.stringify(this.b))
     },
       error => {
         this.msg = error.error;
-        console.log("msg "+this.msg)
+        console.log("msg " + this.msg)
       }
     );
   }
-
 
   addBook(b: Book): Observable<Book> {
     return this.service.addbook(b);
