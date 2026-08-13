2e1cac8d939688dd84d792c50492020c8d4b7336
shankar-trainer <shankar7979@hotmail.com>
2026-08-13 12:09:55 +0530

    added

--- spring-boot/ms/apigateway/src/main/java/com/coforge/ApiGatewayMain.java
+++ spring-boot/ms/apigateway/src/main/java/com/coforge/ApiGatewayMain.java
@@ -7,8 +7,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayMain {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayMain.class, args);
     }
 }
+
+//http://localhost:9090/product
+//http://localhost:8082/product -- using api-gateway
+
+//http://localhost:8082/order
+//http://localhost:7070/order
--- spring-boot/ms/product-order/src/main/java/com/coforge/OrderMain.java
+++ spring-boot/ms/product-order/src/main/java/com/coforge/OrderMain.java
@@ -12,7 +12,6 @@ public class OrderMain {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
-
     }
 
 }
