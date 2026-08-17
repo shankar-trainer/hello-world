8b0dd5850b5f3e87472810254d6cfd8098bd312c
shankar-trainer <shankar7979@hotmail.com>
2026-08-17 10:23:26 +0530

    added

--- spring-boot/ms/example2-withcrossorigin/user-service/src/main/java/org/example/ExampleController.java
+++ spring-boot/ms/example2-withcrossorigin/user-service/src/main/java/org/example/ExampleController.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/example")
@@ -12,4 +13,11 @@ public class ExampleController {
     public String getExample() {
         return "Hello from Example Service!";  // Response to GET requests
     }
+//
+//    @GetMapping("/example")
+//    public Mono<String> example() {
+//        return Mono.just("ok");
+//    }
+
 }
+
--- spring-boot/ms/ms1/readme.txt
+++
@@ -1,2 +0,0 @@
-http://localhost:8086/client
-http://localhost:9056/client
